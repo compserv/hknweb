d2fd11e859e3dc4840ad410a168daecd57ca51c5
James Zhu <james.zhu.engineer@gmail.com>
2017-11-27 22:11:20 -0800

    Redirect /events (index page) to /events/calendar

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.calendar),
+    url(r'^$', views.index),
     url(r'^calendar', views.calendar, name='calendar'),
     url(r'^future', views.future, name='future'),
     url(r'^past', views.past, name='past'),
--- events/views.py
+++ events/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+
+def index(request):
+    return redirect('calendar')
 
 
 def calendar(request):
