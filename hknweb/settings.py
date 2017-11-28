69f168d8f1d5da36ddeff9bbefc0d4360b09a935
James Zhu <james.zhu.engineer@gmail.com>
2017-11-27 22:56:46 -0800

    Add project-wide static file directory

--- hknweb/settings.py
+++ hknweb/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
