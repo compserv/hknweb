6e4c1da4c23e896b6ef75ff82174e61c11658163
James Zhu <james.zhu.engineer@gmail.com>
2017-11-27 20:39:27 -0800

    Add MySQL config (without auth details)

--- hknweb/settings.py
+++ hknweb/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'hknweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'read_default_file': './db.cnf'
+        }
     }
 }
 
