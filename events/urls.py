f07cd1aa75618920ec8b4aa07b91c78ecb130d48
James Zhu <james.zhu.engineer@gmail.com>
2017-11-27 22:08:05 -0800

    Add events routing (cal, future / past events, rsvps)

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.calendar),
+    url(r'^calendar', views.calendar, name='calendar'),
+    url(r'^future', views.future, name='future'),
+    url(r'^past', views.past, name='past'),
+    url(r'^rsvps', views.rsvps, name='rsvps'),
 ]
--- events/views.py
+++ events/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the views index.")
+def calendar(request):
+    return HttpResponse("Hello, world. You're at the calendar index.")
+
+
+def future(request):
+    return HttpResponse("Hello, world. You're at the future index.")
+
+
+def past(request):
+    return HttpResponse("Hello, world. You're at the past index.")
+
+
+def rsvps(request):
+    return HttpResponse("Hello, world. You're at the rsvps index.")
