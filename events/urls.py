02a2956e79668d0026aaed603763e8ca9dfffcd1
James Zhu <james.zhu.engineer@gmail.com>
2017-11-27 22:13:47 -0800

    Add end anchor to events url regexes

--- events/urls.py
+++ events/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^calendar', views.calendar, name='calendar'),
-    url(r'^future', views.future, name='future'),
-    url(r'^past', views.past, name='past'),
-    url(r'^rsvps', views.rsvps, name='rsvps'),
+    url(r'^calendar/$', views.calendar, name='calendar'),
+    url(r'^future/$', views.future, name='future'),
+    url(r'^past/$', views.past, name='past'),
+    url(r'^rsvps/$', views.rsvps, name='rsvps'),
 ]
