f0100a4843e902954035d918ae94516519b90c32
mhwangc <mhwangc@berkeley.edu>
2017-11-28 23:46:01 -0800

    create form classes and basic account settings page

--- hknweb/urls.py
+++ hknweb/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import hknweb.views as views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^account-settings/', views.account_settings)
 ]
