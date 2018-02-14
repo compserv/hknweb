1b78ea0035680c4777fe93ba9e37d8a78a11e888
James Zhu <jameszhu@berkeley.edu>
2018-02-13 23:23:50 -0800

    Switch to mysqlclient driver
    See https://github.com/compserv/hknweb/pull/1#discussion_r168092425.

--- manage.py
+++ manage.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
-import pymysql
 
 if __name__ == "__main__":
-    # Expose MySQL interface to Django: https://stackoverflow.com/a/34778155
-    pymysql.install_as_MySQLdb()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hknweb.settings")
     try:
         from django.core.management import execute_from_command_line
