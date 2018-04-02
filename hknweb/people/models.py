0088e2f31d565471eed16d02b97b17b581a93702
bernie wang <berniewang@berkeley.edu>
2018-04-02 00:01:00 -0700

    removed login related fields from People model

--- hknweb/people/models.py
+++ hknweb/people/models.py
@@ -6,11 +6,6 @@ class People(models.Model):
     last_name           = models.CharField(max_length=255, null=False)
     username            = models.CharField(max_length=255, null=False)
     email               = models.CharField(max_length=255, null=False)
-    crypted_password    = models.CharField(max_length=255, null=False)
-    password_salt       = models.CharField(max_length=255, null=False)
-    persistence_token   = models.CharField(max_length=255, null=False)
-    single_access_token = models.CharField(max_length=255, null=False)
-    perishable_token    = models.CharField(max_length=255, null=False)
     phone_number        = models.CharField(max_length=255)
     date_of_birth       = models.DateField()
     created_at          = models.DateTimeField(auto_now_add=True)
@@ -21,10 +16,6 @@ class People(models.Model):
     perm_address        = models.CharField(max_length=255, default="")
     grad_semester       = models.CharField(max_length=255, default="")
     approved            = models.BooleanField()
-    failed_login_count  = models.IntegerField(default=0, null=False)
-    current_login_at    = models.DateTimeField()
-    reset_password_link = models.CharField(max_length=255)
-    reset_password_at   = models.DateTimeField()
     graduation          = models.CharField(max_length=255)
 
 
--- hknweb/settings.py
+++ hknweb/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'hknweb',
     'hknweb.events',
+    'hknweb.people',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
