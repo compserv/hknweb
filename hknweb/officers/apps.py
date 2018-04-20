11ce1591ebfda1efae09dec987952b42a85d2e51
James Zhu <jameszhu@berkeley.edu>
2018-04-20 14:20:57 -0700

    Copy events template to officers

---
+++ hknweb/officers/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OfficersConfig(AppConfig):
+    name = 'officers'
---
+++ hknweb/officers/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import loader
+
+# from .models import Event
+
+def index(request):
+    # events = Event.objects.order_by('-created_at')[:10]
+
+    context = {
+        # 'events': events,
+    }
+    return render(request, 'events/index.html', context)
+
+
+# def future(request):
+#     return HttpResponse("Hello, world. You're at the future index.")
+
+
+# def past(request):
+#     return HttpResponse("Hello, world. You're at the past index.")
+
+
+# def rsvps(request):
+#     return HttpResponse("Hello, world. You're at the rsvps index.")
