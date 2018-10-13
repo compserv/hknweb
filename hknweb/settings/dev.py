57db78195322f5e9d2a7d24976e2cdcfbebb21cb
George <geohhutch@gmail.com>
2018-10-13 01:26:16 +0000

    Added Cloud9 testsite url to ALLOWED_HOSTS for settings-mode 'dev'

--- hknweb/settings/dev.py
+++ hknweb/settings/dev.py
@@ -9,7 +9,7 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','hkn.eecs.berkely.edu','hkn.mu']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','hkn.eecs.berkely.edu','hkn.mu','hknweb-geohh.c9users.io']
 
 
 
