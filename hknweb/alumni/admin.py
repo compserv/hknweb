3bf972b30e67f4882c11920a0b1d4ac5b653ffa3
alexander-zw <alexanderwu68@yahoo.com>
2018-11-04 22:44:07 -0800

    Make more small changes to model

--- hknweb/alumni/admin.py
+++ hknweb/alumni/admin.py
@@ -5,7 +5,8 @@ from .models import Alumnus
 class AlumnusAdmin(admin.ModelAdmin):
     fields = ['first_name', 'last_name', 'perm_email', 'mailing_list',
               'grad_semester', 'grad_school', 'job_title', 'company',
-              'salary', 'created_at', 'updated_at', 'location']
+              'salary', 'created_at', 'updated_at', 'location',
+              'suggestions']
     # inlines = [ChoiceInline]
     list_display = ('first_name', 'last_name', 'perm_email')
     list_filter = ['grad_semester', 'mailing_list', 'created_at',
