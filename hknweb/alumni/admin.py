1c55bbb474684a9cda50fda95054120d0ae6eb0c
alexander-zw <alexanderwu68@yahoo.com>
2018-11-04 22:33:29 -0800

    Set default values for alumnus model and migrate

--- hknweb/alumni/admin.py
+++ hknweb/alumni/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Alumnus
 
-# Register your models here.
+
+class AlumnusAdmin(admin.ModelAdmin):
+    fields = ['first_name', 'last_name', 'perm_email', 'mailing_list',
+              'grad_semester', 'grad_school', 'job_title', 'company',
+              'salary', 'created_at', 'updated_at', 'location']
+    # inlines = [ChoiceInline]
+    list_display = ('first_name', 'last_name', 'perm_email')
+    list_filter = ['grad_semester', 'mailing_list', 'created_at',
+                   'updated_at']
+    search_fields = ['first_name', 'last_name', 'perm_email']
+
+
+admin.site.register(Alumnus, AlumnusAdmin)
+
--- hknweb/alumni/models.py
+++ hknweb/alumni/models.py
@@ -1,26 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
 
 max_strlen = 255
 
 
 class Alumnus(models.Model):
-    name_id         = models.IntegerField()
-    first_name      = models.CharField(max_length=max_strlen)
-    last_name       = models.CharField(max_length=max_strlen)
-    perm_email      = models.EmailField()
-    mailing_list    = models.BooleanField()
-    grad_semester   = models.CharField(max_length=max_strlen)
-    grad_school     = models.CharField(max_length=max_strlen)
-    job_title       = models.CharField(max_length=max_strlen)
-    company         = models.CharField(max_length=max_strlen)
-    salary          = models.IntegerField()
-    created_at      = models.DateTimeField()
-    updated_at      = models.DateTimeField()
-    location        = models.CharField(max_length=max_strlen)
+    name_id         = models.IntegerField(default=-1)
+    first_name      = models.CharField(max_length=max_strlen, default='')
+    last_name       = models.CharField(max_length=max_strlen, default='')
+    perm_email      = models.EmailField(default='')
+    mailing_list    = models.BooleanField(default='')
+    grad_semester   = models.CharField(max_length=max_strlen, default='')
+    grad_school     = models.CharField(max_length=max_strlen, default='')
+    job_title       = models.CharField(max_length=max_strlen, default='')
+    company         = models.CharField(max_length=max_strlen, default='')
+    salary          = models.IntegerField(default=-1)
+    created_at      = models.DateTimeField(default=timezone.now)
+    updated_at      = models.DateTimeField(default=timezone.now)
+    location        = models.CharField(max_length=max_strlen, default='')
 
     def generate_grad_semester(self, semester, year):
         return semester + ' ' + year
 
+    @property
+    def name(self):
+        return self.first_name + " " + self.last_name
+
     def __str__(self):
-        return self.perm_email
+        return self.name
