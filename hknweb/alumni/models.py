1b0fb8653a2fe9e3aaf84ac0ef5ac4203c49c091
alexander-zw <alexanderwu68@yahoo.com>
2018-11-04 22:11:06 -0800

    Add more model stuff

--- hknweb/alumni/models.py
+++ hknweb/alumni/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 
 
-max_strlen = 200
+max_strlen = 255
 
 
 class Alumnus(models.Model):
-    first_name = models.CharField(max_length=max_strlen)
-    last_name = models.CharField(max_length=max_strlen)
-    perm_email = models.EmailField()
-    mailing_list = models.BooleanField()
-    grad_semester = models.CharField(max_length=max_strlen)
-    grad_school = models.CharField(max_length=max_strlen)
-    job_title = models.CharField(max_length=max_strlen)
-    company = models.CharField(max_length=max_strlen)
-    salary = models.IntegerField()
-    person_id = models.IntegerField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    location = models.CharField(max_length=max_strlen)
+    name_id         = models.IntegerField()
+    first_name      = models.CharField(max_length=max_strlen)
+    last_name       = models.CharField(max_length=max_strlen)
+    perm_email      = models.EmailField()
+    mailing_list    = models.BooleanField()
+    grad_semester   = models.CharField(max_length=max_strlen)
+    grad_school     = models.CharField(max_length=max_strlen)
+    job_title       = models.CharField(max_length=max_strlen)
+    company         = models.CharField(max_length=max_strlen)
+    salary          = models.IntegerField()
+    created_at      = models.DateTimeField()
+    updated_at      = models.DateTimeField()
+    location        = models.CharField(max_length=max_strlen)
 
     def generate_grad_semester(self, semester, year):
         return semester + ' ' + year
