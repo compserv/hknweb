9a56dbaa64de192e99817ba9cd568edc381bc733
alexander-zw <alexanderwu68@yahoo.com>
2018-11-04 20:06:25 -0800

    Add some views to alumni

--- hknweb/alumni/views.py
+++ hknweb/alumni/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.views import generic
+from django.shortcuts import render, redirect
+
+
+# class IndexView(generic.TemplateView):
+#     template_name = 'alumni/index.html'
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the root index.")
+    context = {
+    }
+    return render(request, 'alumni/index.html', context)
 
 
 def form(request):
