aa6f9146f1d11b90e2701ee37807dcf8ec411cc6
alexander-zw <alexanderwu68@yahoo.com>
2018-11-04 21:10:01 -0800

    Move alumni templates and start adding models

--- hknweb/alumni/models.py
+++ hknweb/alumni/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Alumnus(models.Model):
+    perm_email = models.EmailField()
+    mailing_list = models.BooleanField()
+    grad_semester = models.CharField()
+    pub_date = models.DateTimeField('date published')
+
+    def grad_semester(self, semester, year):
+        return semester + ' ' + year
+
+    was_published_recently.admin_order_field = 'pub_date'
+        was_published_recently.boolean = True
+        was_published_recently.short_description = 'Published recently?'
+
+        def __str__(self):
+            return self.question_text
