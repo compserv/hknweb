2ba24247a166f994d184dff17bb20b38f1ae6750
alexander-zw <alexanderwu68@yahoo.com>
2018-11-07 20:41:34 -0800

    Push a broken version for Christian

--- hknweb/alumni/urls.py
+++ hknweb/alumni/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import re_path
 
 from . import views
 
 app_name = 'alumni'
 urlpatterns = [
-    path(r'^form_success/$', views.FormViewSuccess, name='form_success'),
-    path(r'^$', views.IndexView.as_view()),
-    path(r'^form/$', views.FormView, name='form'),
+    re_path(r'^form_success/$', views.FormViewSuccess, name='form_success'),
+    re_path(r'^$', views.IndexView.as_view()),
+    re_path(r'^form/$', views.FormView, name='form'),
 ]
