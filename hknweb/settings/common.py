82ddba83353eef2ba9c3ccd4eec9b80957dfe193
David <dyi@berkeley.edu>
2018-11-07 20:22:09 -0800

    Made changes to common.py in order to add shortlinks to list of apps. Currently, shortlinks are under 's/name of shortlink'

--- hknweb/settings/common.py
+++ hknweb/settings/common.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'hknweb',
     'hknweb.events',
     'hknweb.tutoring',
+    'hknweb.shortlinks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
