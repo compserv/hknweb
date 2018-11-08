cbf4ec7b105c5201217a3e1abe73f54c5ec88150
alexander-zw <alexanderwu68@yahoo.com>
2018-11-07 21:57:18 -0800

    Add search page

--- hknweb/alumni/urls.py
+++ hknweb/alumni/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = 'alumni'
 urlpatterns = [
-    re_path(r'^form_success/$', views.FormViewSuccess, name='form_success'),
-    re_path(r'^$', views.IndexView.as_view()),
+    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^search/$', views.SearchView.as_view(), name='search'),
     re_path(r'^form/$', views.FormView, name='form'),
+    re_path(r'^form_success/$', views.FormViewSuccess, name='form_success'),
 ]
--- hknweb/alumni/views.py
+++ hknweb/alumni/views.py
@@ -1,12 +1,43 @@
 from django.http import HttpResponse
 from django.views import generic
 from django.shortcuts import render, redirect
+from django.db.models import Q
 
+from .models import Alumnus
 from .forms import AlumniForm
+import operator
+from functools import reduce
 
 
 class IndexView(generic.TemplateView):
     template_name = 'alumni/index.html'
+
+
+class SearchView(generic.ListView):
+    """
+    Display an alumni list page filtered by the search query.
+    """
+    template_name = 'alumni/search.html'
+    context_object_name = 'matching_alumni_list'
+    paginate_by = 10
+
+    def get_queryset(self):
+        result = Alumnus.objects
+
+        query = self.request.GET.get('q')
+        if not query:
+            return []
+
+        print(query)
+        query_list = query.split()
+        result = result.filter(
+            reduce(operator.and_,
+                   (Q(first_name__icontains=q) for q in query_list)) |
+            reduce(operator.and_,
+                   (Q(city__icontains=q) for q in query_list))
+        )
+        print(result)
+        return result
 
 
 def FormView(request):
@@ -29,3 +60,4 @@ def FormViewSuccess(request):
             else:
                 return render(request, 'alumni/form.html', {'form':form})
     return render(request, 'alumni/form_success.html', {'form':form})
+
