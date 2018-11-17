cc62b145c9d3ba6e1f52ecafa8600a8aa45a007b
James Zhu <jameszhu@berkeley.edu>
2018-11-16 16:57:20 -0800

    Remove officers folder

--- hknweb/officers/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Committee
-from .models import Officer
-
-admin.site.register(Committee)
-admin.site.register(Officer)
--- hknweb/officers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OfficersConfig(AppConfig):
-    name = 'officers'
