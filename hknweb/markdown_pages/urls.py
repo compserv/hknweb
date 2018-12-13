30f58c6ed528c359d4bf19809528145a0da2089e
James Zhu <jameszhu@berkeley.edu>
2018-12-12 22:57:46 -0800

    Fix markdown_pages urls

--- hknweb/markdown_pages/urls.py
+++ hknweb/markdown_pages/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url, include
+from django.urls import path
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.editor),
-    url(r'^(?P<path>[\w\d\.\-_]+)/', views.display)
+    path('', views.editor),
+    re_path(r'^(?P<path>[\w\d\.\-_]+)/', views.display),
 ]
