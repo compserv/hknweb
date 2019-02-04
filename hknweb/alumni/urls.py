72e829dee321ebbf82ebe3830dc450f5c29633ed
alexander-zw <alexanderwu68@yahoo.com>
2019-02-04 12:28:44 -0800

    Fix Alumni paths and migrate

--- hknweb/alumni/urls.py
+++ hknweb/alumni/urls.py
@@ -1,14 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 app_name = 'alumni'
 urlpatterns = [
     # When doing the officer challenge feature, I changed all these from
-    # re_path to path
+    # re_path to path, except detail/
     path('', views.IndexView.as_view(), name='index'),
     path('search/', views.SearchView.as_view(), name='search'),
-    path('detail/(?P<oid>\d+)/', views.alumni_detail_view, name='detail'),
+    # matches only valid ids
+    re_path(r'^detail/(?P<oid>\d+)/$', views.alumni_detail_view, name='detail'),
     path('search_type/', views.search_type, name='search_type'),
     path('form/', views.form, name='form'),
 ]
