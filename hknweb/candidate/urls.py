9884078d738a7b4317dd6493d01c5283ec5d04a0
alexander-zw <alexanderwu68@yahoo.com>
2019-02-03 21:48:15 -0800

    Add template folder and some urls

--- hknweb/candidate/urls.py
+++ hknweb/candidate/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = 'alumni'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    # candidate end of officer challenge requests
+    path('candrequests', views.IndexView.as_view(), name='candrequests'),
+    # officer end of officer challenge requests
+    path('offrequests', views.IndexView.as_view(), name='offrequests'),
 ]
