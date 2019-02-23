2f42fe4015c29c1a7c11facda731aefd2851b3b1
Alexander Wu <alexanderwu@airbears2-10-142-32-190.airbears2.1918.berkeley.edu>
2019-02-22 19:16:21 -0800

    Fix migrations and email

--- hknweb/settings/common.py
+++ hknweb/settings/common.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import os
+from .secrets import EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
