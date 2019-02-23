36a67b24eac4695f15e458938b55c9e75bce1ae3
Alexander Wu <alexanderwu@airbears2-10-142-32-190.airbears2.1918.berkeley.edu>
2019-02-22 18:44:07 -0800

    Fix merge conflict

--- hknweb/settings/prod.py
+++ hknweb/settings/prod.py
@@ -1,9 +1,7 @@
 from .common import *
 
-
-#In prod mode, rigidly enforce using real secrets and fail if unavailiable
+# In prod mode, rigidly enforce using real secrets and fail if unavailiable
 from .secrets import *
-
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'v9lj^szduvr@a*31&r(l5ub+5q%ebszts70vlpzaiekt23s)gb'
@@ -11,4 +9,9 @@ SECRET_KEY = 'v9lj^szduvr@a*31&r(l5ub+5q%ebszts70vlpzaiekt23s)gb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['hkn.eecs.berkeley.edu','hkn.mu']
+ALLOWED_HOSTS = [
+    'hkn.eecs.berkeley.edu',
+    'hkn.mu',
+    'dev-hkn-eecs-berkeley-edu.apphost.ocf.berkeley.edu',
+    'dev-hkn.eecs.berkeley.edu',
+]
