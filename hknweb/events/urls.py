79712ced40badf9f3328657f65d2e030f7dc40dc
carolynwang <carolyn.wang@berkeley.edu>
2019-02-28 16:03:56 -0800

    candidate portal things

--- hknweb/events/urls.py
+++ hknweb/events/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
+app_name = 'events'
 urlpatterns = [
     path(r'<int:id>', views.show_details),
     path(r'<int:id>/rsvp', views.rsvp),
     path(r'<int:id>/unrsvp', views.unrsvp),
     path('new', views.add_event),
     path('checklist', views.show_checklist),
-    path(r'', views.index),
+    path(r'', views.index, name='index'),
     # url(r'^future/$', views.future, name='future'),
     # url(r'^past/$', views.past, name='past'),
 ]
