6de2887eb3c878ed714e6207aef8fe3a9615e637
Alexander Wu <alexanderwu@airbears2-10-142-33-94.airbears2.1918.berkeley.edu>
2019-03-22 12:47:52 -0700

    What have you done carolyn where's my email password

--- hknweb/settings/common.py
+++ hknweb/settings/common.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import os
 from django.conf.global_settings import DATETIME_INPUT_FORMATS
-#from .secrets import EMAIL_HOST_PASSWORD
+from .secrets import EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
