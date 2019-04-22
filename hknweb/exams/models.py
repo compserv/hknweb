71f1eaf1f64253ad70a5dab00f9e20882a1c330c
Anthony Ding <anthonyding@berkeley.edu>
2019-04-21 18:22:11 -0700

    Modified html and instructor release

--- hknweb/exams/models.py
+++ hknweb/exams/models.py
@@ -10,13 +10,11 @@ class Department(models.Model):
 class Instructor(models.Model):
     name = models.CharField(max_length=255)
     department = models.ForeignKey(Department, on_delete=models.CASCADE, null=False)
-    release = models.BooleanField()
 
     def __str__(self):
-        return "Instructor(name={}, department={}, release={})".format(self.name, self.department, self.release)
+        return "Instructor(name={}, department={})".format(self.name, self.department)
 
 class Course(models.Model):
-    #id          = models.IntegerField(primary_key=True)
     name        = models.CharField(max_length=255, null=False)
     department  = models.ForeignKey(Department, on_delete=models.CASCADE, null=False)
     number 		= models.CharField(max_length=10, null=False)
@@ -25,10 +23,10 @@ class Course(models.Model):
         return "{} {}".format(self.department, self.number)
 
 class CourseSemester(models.Model):
-    #id          = models.IntegerField(primary_key=True)
     course      = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
     semester    = models.CharField(max_length=255)
     instructor  = models.ForeignKey(Instructor, on_delete=models.CASCADE, null=False)
+    release     = models.BooleanField()
     midterm1    = models.FileField(blank=True)
     midterm1_sol = models.FileField(blank=True)
     midterm2    = models.FileField(blank=True)
