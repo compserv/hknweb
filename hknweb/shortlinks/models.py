5321e9bc465e9ab30c62a407a20aea5917e1644c
Shemuel Ong <shemuelong@berkeley.edu>
2019-04-28 13:51:00 -0700

    Rename Links.leader to Links.creator

--- hknweb/shortlinks/models.py
+++ hknweb/shortlinks/models.py
@@ -5,5 +5,7 @@ class Links(models.Model):
         verbose_name_plural = "Links" #fix plural without using Meta class
     name = models.CharField(max_length=255, null=False, unique=True)
     redirect = models.URLField()
-    leader = models.CharField(max_length=255, null=False)
+    creator = models.CharField(max_length=255, null=False)
 
+    def __str__(self):
+       return "{} created by {}".format(self.name, self.creator)
