7b762b72a9b289a8aa7a0b8937e6c87d2cfc7c20
Matthew Signorotti <matthew.signorotti@berkeley.edu>
2019-05-01 01:01:13 -0700

    Improve homepage formatting and query upcoming events from the database

--- hknweb/views/landing.py
+++ hknweb/views/landing.py
@@ -1,15 +1,40 @@
-from django.shortcuts import redirect
-from django.http import HttpResponse
-from django.http import JsonResponse
-from hknweb.models import Profile
-# import ast
-from django.contrib.auth.models import User
-from hknweb.forms import SettingsForm, ProfileForm
-from django.contrib.auth import authenticate
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render
-
+from hknweb.events.models import Event
+from hknweb.tutoring.models import Tutor
+import datetime
 
 def home(request):
-    return render(request, 'landing/home.html')
+    today = datetime.datetime.now()
+    next_weekday = today
+    # TODO: determine earliest weekday for which tutoring still has yet to complete, and query those tutors
+    # tutors = 
+    
+    weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    def two_digits(minutes):
+        if len(minutes) == 1:
+            return '0' + minutes
+        return minutes
+    def parse_date(date_time_obj):
+        start, end = date_time_obj.start_time, date_time_obj.end_time
+        start_timetuple, end_timetuple = start.timetuple(), end.timetuple()
+        day = weekdays[start_timetuple[6]] + ' ' + str(start_timetuple[1]) + '/' + str(start_timetuple[2])
+        start_time = str(start_timetuple[3]) + ':' + two_digits(str(start_timetuple[4]))
+        if start_timetuple[2] == end_timetuple[2]:
+            end_time = str(end_timetuple[3]) + ':' + two_digits(str(end_timetuple[4]))
+        else:
+            end_time = 'overnight'
+        return {'day': day, 'start': start_time, 'end': end_time}
+    upcoming_events = Event.objects.filter(start_time__gte=today).order_by('start_time')
+    if len(upcoming_events) > 4:
+        upcoming_events = upcoming_events[:4]
+    events = []
+    for event in upcoming_events:
+        temp_dict = parse_date(event)
+        temp_dict['name'] = event.name
+        events.append(temp_dict)
+    
+    context = {
+        # 'tutors': tutors,
+        'events': events,
+    }
+    return render(request, 'landing/home.html', context)
