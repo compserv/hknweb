d7a29816bb27c2fee52596044cab0665c3fa6208
Alexander Wu <alexanderwu@alex-eecs.local>
2019-07-07 16:17:03 -0700

    Reorganize file structure

--- hknweb/candidate/views.py
+++ hknweb/candidate/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.views import generic
 from django.views.generic.edit import FormView, UpdateView
 from django.shortcuts import render, redirect, reverse
