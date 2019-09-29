0f08583a358cbed6f334baed85652ae2eefeda41
Alexander Wu <alexanderwu@alex.eecs>
2019-09-29 15:42:59 -0700

    Remove unneeded import

--- hknweb/settings/common.py
+++ hknweb/settings/common.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import os
 from django.conf.global_settings import DATETIME_INPUT_FORMATS
-from .secrets import EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
