a3dfef89316e3f320132b4f824b5c94f6f510766
Alexander Wu <alexanderwu68@yahoo.com>
2019-10-19 16:43:55 -0700

    Remove duplicate line

--- hknweb/settings/common.py
+++ hknweb/settings/common.py
@@ -78,7 +78,6 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'social_django.context_processors.backends',
                 'social_django.context_processors.login_redirect',
-                'social_django.context_processors.login_redirect',
                 'hknweb.views.users.add_officer_context',
             ],
         },
