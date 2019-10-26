7261983e6d86d09d41be84f1eb285c27095ff238
Alexander Wu <alexanderwu68@yahoo.com>
2019-10-25 20:27:55 -0700

    Add admin actions to confirm or reject challenges in bulk (csec)

--- hknweb/candidate/admin.py
+++ hknweb/candidate/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import OffChallenge, Announcement
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+
+from .models import OffChallenge, Announcement
+from .views import send_cand_confirm_email
 
 import csv
 
@@ -14,17 +16,27 @@ class OffChallengeAdmin(admin.ModelAdmin):
     list_filter = ['requester', 'officer', 'officer_confirmed', 'csec_confirmed', 'request_date']
     search_fields = ['requester', 'officer', 'name']
 
-    actions = ["export_as_csv"]
+    actions = ["export_as_csv", "csec_confirm", "csec_reject"]
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "officer":
             kwargs["queryset"] = User.objects.all().order_by('username')
         return super(OffChallengeAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
-    # TODO
     def save_model(self, request, obj, form, change):
-        obj.user = request.user
         super().save_model(request, obj, form, change)
+        if 'csec_confirmed' in form.changed_data:
+            OffChallengeAdmin.check_send_email(request, obj)
+
+    def check_send_email(request, obj):
+        # officer has already confirmed, and now csec confirms
+        if obj.csec_confirmed is True and obj.officer_confirmed is True:
+            send_cand_confirm_email(request, obj, True)
+        # officer has not already rejected, and now csec rejects
+        elif obj.csec_confirmed is False and obj.officer_confirmed is not False:
+            send_cand_confirm_email(request, obj, False)
+        # if neither is true, either need to wait for officer to review,
+        # or officer has already rejected
 
     # @source: http://books.agiliq.com/projects/django-admin-cookbook/en/latest/export.html
     def export_as_csv(self, request, queryset):
@@ -37,11 +49,29 @@ class OffChallengeAdmin(admin.ModelAdmin):
 
         writer.writerow(field_names)
         for obj in queryset:
-            row = writer.writerow([getattr(obj, field) for field in field_names])
+            writer.writerow([getattr(obj, field) for field in field_names])
 
         return response
 
     export_as_csv.short_description = "Export selected as csv"
+
+    def csec_confirm(self, request, queryset):
+        for obj in queryset:
+            if obj.csec_confirmed is not True:
+                obj.csec_confirmed = True
+                obj.save()
+                OffChallengeAdmin.check_send_email(request, obj)
+
+    csec_confirm.short_description = "Mark selected as confirmed (csec)"
+
+    def csec_reject(self, request, queryset):
+        for obj in queryset:
+            if obj.csec_confirmed is not False:
+                obj.csec_confirmed = False
+                obj.save()
+                OffChallengeAdmin.check_send_email(request, obj)
+
+    csec_reject.short_description = "Mark selected as rejected (csec)"
 
 
 class AnnouncementAdmin(admin.ModelAdmin):
--- hknweb/candidate/models.py
+++ hknweb/candidate/models.py
@@ -23,7 +23,9 @@ class OffChallenge(models.Model):
 
     # the requester needs permission to add officer challenges. This limits the choices in a dropdown,
     # but the view CandRequestView is still able to set requesters regardless of their permissions
-    requester         = models.ForeignKey('auth.User', limit_choices_to={'groups__permissions__codename': 'add_offchallenge'},
+    requester         = models.ForeignKey('auth.User',
+    # NOTE: for some reason this causes an error when you try to save on the admin, so I'll just comment it out for now
+                            # limit_choices_to=Q(groups__permissions__codename='add_offchallenge'),
                             on_delete=models.CASCADE, default=None, related_name='requester')
     officer           = models.ForeignKey('auth.User', limit_choices_to={'groups__name': settings.OFFICER_GROUP},
                             on_delete=models.CASCADE, default=None, related_name='officer')
