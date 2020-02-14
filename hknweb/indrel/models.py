2ab996210e4adc075b21598e44dd4bb67436379a
bri25yu <bri25yu@gmail.com>
2020-02-13 21:29:42 -0800

    created basic company model

--- hknweb/indrel/models.py
+++ hknweb/indrel/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    company_name = models.CharField(max_length = 200)
+    address_1 = models.CharField(max_length = 200)
+    city = models.CharField(max_length = 200)
+    state = models.CharField(max_length = 200)
+    zipcode = models.IntegerField()
+    contact_name = models.CharField(max_length = 200)
+    contact_title = models.CharField(max_length = 200)
+    contact_phone = models.CharField(max_length = 200)
+    contact_email = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return str(self.company_name)
+
+    def __repr__(self):
+        return ''.join(str(self.company_name).split()).lower()
