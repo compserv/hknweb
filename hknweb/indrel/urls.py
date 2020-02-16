b5203300b86eeb0259bdb0997dd38bcfb873b38f
bri25yu <bri25yu@gmail.com>
2020-02-16 11:58:38 -0800

    adding urls for resume book ordering, infosession registration

--- hknweb/indrel/urls.py
+++ hknweb/indrel/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'indrel'
 urlpatterns = [
     path('', views.index),
-    path('resume-book', views.resume_book),
-    path('resume-book/order', views.resume_book_order_form),
-    path('infosessions', views.infosessions),
-    path('infosession/registration', views.infosessions_registration),
+    path('resume-book', views.resume_book, name="resume_book"),
+    path('resume-book/order', views.ResumeBookOrderFormView.as_view(), name="resume_book/order"),
+    path('infosessions', views.infosessions, name="infosessions"),
+    path('infosessions/registration', views.InfosessionRegistrationView.as_view(), name="infosessions/registration"),
     path('career-fair', views.career_fair),
     path('contact-us', views.contact_us, name="contact_us"),
 ]
--- hknweb/indrel/views.py
+++ hknweb/indrel/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import ResumeBookOrderForm, InfosessionRegistration
 
 def index(request):
     return render(request, 'indrel/index.html')
@@ -15,11 +17,13 @@ def career_fair(request):
 def contact_us(request):
     return render(request, 'indrel/contact_us.html')
 
-def resume_book_order_form(request):
-    return render(request, 'indrel/resume_book_order_form.html')
+class ResumeBookOrderFormView(generic.DetailView):
+    model = ResumeBookOrderForm
+    template_name = 'indrel/resume_book_order_form.html'
 
-def infosessions_registration(request):
-    return render(request, 'indrel/infosessions_registration.html')
+class InfosessionRegistrationView(generic.DetailView):
+    model = InfosessionRegistration
+    template_name = 'indrel/infosessions_registration.html'
 
 def order_resume_book(request):
     pass
