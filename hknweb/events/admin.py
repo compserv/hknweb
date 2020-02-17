cee2afc6c3f04a1319243b7ef81e3c861b1d0d77
Alexander Wu <alexanderwu68@yahoo.com>
2020-02-16 18:50:15 -0800

    Fix duplicate field in EventAdmin

--- hknweb/events/admin.py
+++ hknweb/events/admin.py
@@ -4,8 +4,8 @@ from .models import EventType, Event, Rsvp
 
 @admin.register(EventType)
 class EventTypeAdmin(admin.ModelAdmin):
-    ordering = ['type']
     search_fields = ['type']
+    ordering = ['type']
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
@@ -16,10 +16,8 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_time', 'location', 'event_type','created_by', 'created_at')
     list_filter = ['start_time', 'created_at', 'event_type', 'location', 'created_by']
     search_fields = ['name', 'created_by__username', 'created_by__first_name', 'created_by__last_name']
-    autocomplete_fields = ['event_type', 'created_by']
-
     ordering = ['-created_at']
-    search_fields = ['name']
+    autocomplete_fields = ['event_type', 'created_by']
 
 
 @admin.register(Rsvp)
@@ -30,6 +28,7 @@ class RsvpAdmin(admin.ModelAdmin):
     list_display = ('event', 'user', 'full_name', 'confirmed', 'created_at')
     list_filter = ['confirmed', 'created_at', 'event']
     search_fields = ['event__name', 'user__username', 'user__first_name', 'user__last_name']
+    ordering = ['-created_at']
     autocomplete_fields = ['event', 'user']
 
     actions = ["mark_confirmed", "mark_unconfirmed", "cute_animal"]
