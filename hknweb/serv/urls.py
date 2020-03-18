faae0dd2abb74b4bcf32542e00886f9276e7cbaf
Anthony Ding <anthonyding@berkeley.edu>
2020-03-17 19:46:28 -0700

    Finished serv static pages

---
+++ hknweb/serv/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('eecsday', views.eecsday),
+    path('jreecs', views.jreecs),
+    path('bearhacks', views.bearhacks),
+    path('makershops', views.maker), 
+    path('calday', views.calday),
+]
