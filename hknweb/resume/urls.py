b146e099e967c4bd3b9e864a1a5026f8aa34d6a7
Warren <warrenwwang@berkeley.edu>
2020-04-26 14:56:48 -0700

    added warning for bad uploads

--- hknweb/resume/urls.py
+++ hknweb/resume/urls.py
@@ -1,8 +1,5 @@
-
-# from django.urls import url
 from django.urls import path
 from . import views
-
 
 app_name = 'resume'
 urlpatterns = [
--- hknweb/resume/views.py
+++ hknweb/resume/views.py
@@ -1,31 +1,7 @@
 from django.shortcuts import render
 from .forms import DocumentForm
-from django.core.files.storage import FileSystemStorage
-
-# Create your views here.
-# def model_form_upload(request):
-#     if request.method == 'POST':
-#         form = DocumentForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
-#     else:
-#         form = DocumentForm()
-#     return render(request, 'core/model_form_upload.html', {
-#         'form': form
-#     })
 
 def index(request):
- #  if request.method == 'POST':
-#         form = DocumentForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
-#     else:
-#         form = DocumentForm()
-#     return render(request, 'core/model_form_upload.html', {
-#         'form': form
-#     })
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         print(form.errors)
@@ -35,17 +11,17 @@ def index(request):
             form.save()
             return render(request, 'resume/uploaded.html')  
         else:
-            print("Form wasnt valid")      
-        #     fs = FileSystemStorage()
-        # filename = fs.save(myfile.name, myfile)
-        # uploaded_file_url = fs.url(filename)
-        # return render(request, 'resume/uploaded.html', {
-        #     'uploaded_file_url': uploaded_file_url
-        # })
+            print("Form wasnt valid")
+            form = DocumentForm()
+            return render(request, 'resume/index.html', {
+                'form': form,
+                'err': True,
+            })
 
     form = DocumentForm()
     return render(request, 'resume/index.html', {
-        'form': form
+        'form': form,
+        'err': False,
     })
 
 
