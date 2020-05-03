a42b714cafdb6ee0565c04037d293decf6a0e10e
Alexander Wu <alexanderwu68@yahoo.com>
2020-05-02 17:47:57 -0700

    Add tooltip in add event form and placeholder for slug

--- hknweb/events/forms.py
+++ hknweb/events/forms.py
@@ -7,32 +7,20 @@ from .models import EventType
 
 class EventForm(forms.ModelForm):
 
-    # start_date = forms.DateTimeField(
-    #     label='Start',
-    #     widget=forms.widgets.DateTimeInput(attrs={'type':'datetime-local'}),
-    # )
-    # end_date = forms.DateTimeField(
-    #     label='End',
-    #     widget=forms.widgets.DateTimeInput(attrs={'type':'datetime-local'})
-    # )
     class Meta:
         model = Event
         fields = ('name', 'slug', 'location', 'description', 'event_type', 'start_time', 'end_time', 'rsvp_limit')
-                  #'markdown', 'event_type', 'view_permission', 'rsvp_type', 'transportation')
+                  # 'markdown', 'event_type', 'view_permission', 'rsvp_type', 'transportation')
 
-        #this makes formatting easier, but it shows as MM/DD/YYY HH:MM AM/PM which apparently is not valid for the datetimefield :(
-        # widgets = {
+        # this makes formatting easier, but it shows as MM/DD/YYY HH:MM AM/PM which apparently is not valid for the datetimefield :(
+        widgets = {
+            'slug': forms.TextInput(attrs={'placeholder': 'e.g. <semester>-<name>'}),
             # 'start_time': forms.widgets.DateTimeInput(attrs={'type':'datetime-local'}),
-        #     # 'end_time': forms.widgets.DateTimeInput(attrs={'type':'datetime-local'})
-        # }
-        # field_classes = {
-        #     'start_time': forms.widgets.DateTimeInput,
-        #     'end_time': forms.widgets.DateTimeInput,
-        # }
+            # 'end_time': forms.widgets.DateTimeInput(attrs={'type':'datetime-local'}),
+        }
         help_texts = {
             'start_time': 'mm/dd/yyyy hh:mm, 24-hour time',
             'end_time': 'mm/dd/yyyy hh:mm, 24-hour time',
-            'slug': 'e.g. <semester>-<name>',
         }
 
         labels = {
