47d0f8b4bce3e99651094f2adebd8e23f740b46f
Alexander Wu <alexanderwu68@yahoo.com>
2020-05-02 18:25:00 -0700

    Add datetime picker for add event form

--- hknweb/events/forms.py
+++ hknweb/events/forms.py
@@ -6,21 +6,14 @@ from .models import EventType
 
 
 class EventForm(forms.ModelForm):
-
     class Meta:
         model = Event
         fields = ('name', 'slug', 'location', 'description', 'event_type', 'start_time', 'end_time', 'rsvp_limit')
-                  # 'markdown', 'event_type', 'view_permission', 'rsvp_type', 'transportation')
 
-        # this makes formatting easier, but it shows as MM/DD/YYY HH:MM AM/PM which apparently is not valid for the datetimefield :(
         widgets = {
             'slug': forms.TextInput(attrs={'placeholder': 'e.g. <semester>-<name>'}),
-            # 'start_time': forms.widgets.DateTimeInput(attrs={'type':'datetime-local'}),
-            # 'end_time': forms.widgets.DateTimeInput(attrs={'type':'datetime-local'}),
-        }
-        help_texts = {
-            'start_time': 'mm/dd/yyyy hh:mm, 24-hour time',
-            'end_time': 'mm/dd/yyyy hh:mm, 24-hour time',
+            'start_time': forms.widgets.DateTimeInput(format='%d/%m/%Y %H:%M'),
+            'end_time': forms.widgets.DateTimeInput(format='%d/%m/%Y %H:%M'),
         }
 
         labels = {
