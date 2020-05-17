e5fa2f3ad896bec9bc99658f9f6df0dabb08b0dd
Connie Huang <conhuang@berkeley.edu>
2020-05-17 14:18:54 -0700

    Finished script to initialize permissions to groups

---
+++ hknweb/init_permissions.py
@@ -0,0 +1,48 @@
+from hknweb.candidate.models import Announcement, OffChallenge, BitByteActivity
+from hknweb.events.models import Event, Rsvp
+from django.conf import settings
+from django.contrib.auth.models import User, Group, Permission
+from django.contrib.contenttypes.models import ContentType
+
+candidate = Group.objects.get_or_create(name='candidate')[0]
+officer = Group.objects.get_or_create(name='officer')[0]
+
+#CANDIDATE AND OFFICER PERMISSIONS
+announcement = ('view_announcement', Announcement)
+add_offchallenge = ('add_offchallenge', OffChallenge)
+view_offchallenge =	('view_offchallenge', OffChallenge)
+change_offchallenge = ('change_offchallenge', OffChallenge)
+add_bitbyteactivity = ('add_bitbyteactivity', BitByteActivity)
+view_event = ('view_event', Event)
+add_rsvp = ('add_rsvp',Rsvp)
+remove_rsvp = ('remove_rsvp',Rsvp)
+view_user = ('view_user', User)
+
+permission_names = [announcement, add_offchallenge, view_offchallenge, \
+	change_offchallenge, add_bitbyteactivity, add_rsvp, remove_rsvp, view_user]
+
+permission_group = {'candidate': permission_names, \
+'officer': []}
+
+# OFFICER ONLY PERMISSIONS
+add_event = ('add_event', Event)
+change_event = ('change_event', Event)
+
+permission_group['officer'].extend([add_event, change_event])
+
+# SETTING PERMISSIONS TO GROUPS
+cand_permissions = []
+for perm in permission_group['candidate']:
+	ct =  ContentType.objects.get_for_model(perm[1])
+	permission = Permission.objects.get(codename=perm[0], content_type=ct)
+	cand_permissions.append(permission)
+
+candidate.permissions.set(cand_permissions)
+officer.permissions.set(cand_permissions)
+
+for perm in permission_group['officer']:
+	ct =  ContentType.objects.get_for_model(perm[1])
+	permission = Permission.objects.get(codename=perm[0], content_type=ct)
+	officer.permissions.add(permission)
+
+
