124872d3819d6f26971758bf3533d5515a00af0d
Oscar Chan <ochan2@berkeley.edu>
2020-06-11 22:02:14 -0700

    Exam Autocomplete - cleaned up

--- hknweb/exams/urls.py
+++ hknweb/exams/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from django.urls import path, re_path
-# from .exam_autocomplete import CourseSemesterAutocomplete
 
 from . import views
 
@@ -8,9 +7,4 @@ urlpatterns = [
 	path('course/<department>/<number>/', views.exams_for_course),
     path('new/', views.add_exam),
     path('', views.index),
-    # re_path(
-    #     r'^courseSemester-autocomplete/$',
-    #     CourseSemesterAutocomplete.as_view(),
-    #     name='courseSemester-autocomplete',
-    # )
 ]
