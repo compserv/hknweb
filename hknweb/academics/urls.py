cd3eec89548e0be82c78b901487506e6797a1691
bri25yu <bri25yu@gmail.com>
2020-07-23 00:15:40 -0700

    implementing basic urls

--- hknweb/academics/urls.py
+++ hknweb/academics/urls.py
@@ -1,4 +1,14 @@
+from django.conf.urls import include, url
+from rest_framework.routers import DefaultRouter
+
+from .views import register_viewsets
+
+
 app_name = 'academics'
+
+router = DefaultRouter()
+register_viewsets(router)
+
 urlpatterns = [
-    
+    url(r'^', include(router.urls))
 ]
