7ef046215d1e113c9ebd78cacd7e474b3e65bc73
bri25yu <bri25yu@gmail.com>
2020-07-23 00:54:21 -0700

    further reduced redundancy in permissions, added auto to datetime fields in base_models

--- hknweb/academics/models/base_models.py
+++ hknweb/academics/models/base_models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class AcademicEntity(models.Model):
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
