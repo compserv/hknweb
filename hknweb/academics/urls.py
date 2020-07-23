393a8ded72ca75b92bc8a51e107e00bde966ef1b
bri25yu <bri25yu@gmail.com>
2020-07-23 00:37:48 -0700

    changing academics urlpatterns to use same patterns as other apps in hknweb

--- hknweb/academics/urls.py
+++ hknweb/academics/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import register_viewsets
@@ -10,5 +10,5 @@ router = DefaultRouter()
 register_viewsets(router)
 
 urlpatterns = [
-    url(r'^', include(router.urls))
+    path('', include(router.urls)),
 ]
