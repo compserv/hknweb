b12e21f7d67d6034d236d6bed698b375ac507bf0
bri25yu <bri25yu@gmail.com>
2020-07-24 02:15:14 -0700

    adding reference link to viewset actions for accessible implementations

--- hknweb/academics/views/base_views.py
+++ hknweb/academics/views/base_views.py
@@ -7,5 +7,7 @@ class AcademicEntityViewSet(viewsets.ModelViewSet):
     """
     A base viewset class that provides default `create()`, `retrieve()`, `update()`,
     `partial_update()`, `destroy()` and `list()` actions.
+
+    See https://www.django-rest-framework.org/api-guide/viewsets/#viewset-actions
     """
     permission_classes = (HasPermissionOrReadOnly,)
