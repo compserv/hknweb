eb439cc162c847ba8555c20af7435075329db4f3
bri25yu <bri25yu@gmail.com>
2020-08-26 22:48:16 -0700

    cleaning up code: adding base.html to poc, updating some node package version, reworking frontend urls

--- hknweb/frontend/src/components/App.js
+++ hknweb/frontend/src/components/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("academics/api/departments")
+    fetch("api/departments")
       .then(response => {
         if (response.status > 400) {
           return this.setState(() => {
--- hknweb/frontend/urls.py
+++ hknweb/frontend/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('academics/departments', views.index),
 ]
