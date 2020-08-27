6ae7a88bb96a63e248f066a500b024b41c7fb56d
bri25yu <bri25yu@gmail.com>
2020-08-26 22:44:14 -0700

    proof of concept integration of django and react

---
+++ hknweb/frontend/src/components/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false,
+      placeholder: "Loading"
+    };
+  }
+
+  componentDidMount() {
+    fetch("academics/api/departments")
+      .then(response => {
+        if (response.status > 400) {
+          return this.setState(() => {
+            return { placeholder: "Something went wrong!" };
+          });
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState(() => {
+          return {
+            data,
+            loaded: true
+          };
+        });
+      });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.data.map(department => {
+          return (
+            <li key={department.id}>
+              {department.name} - {department.abbr}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default App;
+
+const container = document.getElementById("app");
+render(<App />, container);
