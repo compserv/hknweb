72c81d10be8fdbfd5c0c659c54dc8bfa5bd2d076
Haolin Zhu <haolinz@berkeley.edu>
2020-09-12 13:44:46 -0700

    Address comments
    Revert changes in Pipfile.lock

--- hknweb/reviewsessions/forms.py
+++ hknweb/reviewsessions/forms.py
@@ -4,27 +4,10 @@ from .models import ReviewSession
 
 class ReviewSessionForm(forms.ModelForm):
 
-    # start_date = forms.DateTimeField(
-    #     label='Start',
-    #     widget=forms.widgets.DateTimeInput(attrs={'type':'datetime-local'}),
-    # )
-    # end_date = forms.DateTimeField(
-    #     label='End',
-    #     widget=forms.widgets.DateTimeInput(attrs={'type':'datetime-local'})
-    # )
     class Meta:
         model = ReviewSession
         fields = ('name', 'slug', 'location', 'description', 'start_time', 'end_time')
 
-        #this makes formatting easier, but it shows as MM/DD/YYY HH:MM AM/PM which apparently is not valid for the datetimefield :(
-        # widgets = {
-            # 'start_time': forms.widgets.DateTimeInput(attrs={'type':'datetime-local'}),
-        #     # 'end_time': forms.widgets.DateTimeInput(attrs={'type':'datetime-local'})
-        # }
-        # field_classes = {
-        #     'start_time': forms.widgets.DateTimeInput,
-        #     'end_time': forms.widgets.DateTimeInput,
-        # }
         help_texts = {
             'start_time': 'mm/dd/yyyy hh:mm, 24-hour time',
             'end_time': 'mm/dd/yyyy hh:mm, 24-hour time',
