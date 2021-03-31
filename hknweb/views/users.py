be6a43b46730d456936e9a3e0daaa53cfb77de6e
ochan1 <oscar4856@yahoo.com>
2021-03-31 03:50:44 -0700

    Profile Settings Different Save Btn for Password and Edit Profile

--- hknweb/views/users.py
+++ hknweb/views/users.py
@@ -75,42 +75,39 @@ def confirm_recaptcha(request):
 def account_settings(request):
     current_user = request.user
     if request.method == 'POST':
-        #user_form = SettingsForm(request.POST, instance = current_user)
         password_form = UpdatePasswordForm(current_user, request.POST)
         profile_form = ProfileForm(request.POST, instance = current_user.profile)
         verify_form = ValidPasswordForm(request.POST, instance = current_user.profile)
-        #user_form.is_valid()
-        #correct_password = authenticate(username=request.POST['username'], password=user_form.cleaned_data['password'])
-        #if not correct_password:
-        #    messages.error(request, ('Incorrect password. You must enter your password to save changes.'))
-        #    return HttpResponseRedirect(request.path_info)
-        #elif user_form.is_valid() and profile_form.is_valid() and password_form.is_valid():
         if verify_form.is_valid():
             correct_password = request.user.check_password(verify_form.data.get('password'))
             if correct_password:
-                if password_form.is_valid():
-                    if password_form.has_changed():
-                        current_user = password_form.save()
-                        update_session_auth_hash(request, current_user)
-                        messages.success(request, ('Your password was successfully updated!'))
+                if "change_password" in request.POST:
+                    if password_form.is_valid():
+                        if password_form.has_changed():
+                            current_user = password_form.save()
+                            update_session_auth_hash(request, current_user)
+                            messages.success(request, ('Your password was successfully updated!'))
+                    else:
+                        messages.error(request, ('Please correct the errors in your Password: {}'.format(list(password_form.errors.values()))))
+                elif "edit_profile" in request.POST:      
+                    if profile_form.is_valid():
+                        if profile_form.has_changed():
+                            #user_form.save()
+                            profile = profile_form.save(commit=False)
+                            profile.user = request.user
+                            profile.save()
+                            #user = password_form.save()
+                            update_session_auth_hash(request, current_user)
+                            messages.success(request, ('Your profile was successfully updated!'))
+                    else:
+                        messages.error(request, ('Please correct the errors in your Profile data: {}'.format(list(profile_form.errors.values()))))
                 else:
-                    messages.error(request, ('Please correct the errors in your Password: {}'.format(list(password_form.errors.values()))))
-
-                if profile_form.is_valid():
-                    if profile_form.has_changed():
-                        #user_form.save()
-                        profile = profile_form.save(commit=False)
-                        profile.user = request.user
-                        profile.save()
-                        #user = password_form.save()
-                        update_session_auth_hash(request, current_user)
-                        messages.success(request, ('Your profile was successfully updated!'))
-                else:
-                    messages.error(request, ('Please correct the errors in your Profile data: {}'.format(list(profile_form.errors.values()))))
+                    messages.error(request, ("Error: Unknown Submission Action"))
             else:
-                messages.error(request, ('Please enter your current password.'))
+                messages.error(request, ('Wrong Password. Please enter your current password.'))
         else:
             messages.error(request, ('Please correct the errors in your Current Password: {}'.format(list(verify_form.errors.values()))))
+        verify_form = ValidPasswordForm(instance = current_user)
         return HttpResponseRedirect(request.path_info)
     else:
         # user_form = SettingsForm(instance = current_user)
