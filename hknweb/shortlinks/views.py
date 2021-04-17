f1b57ce25f107232260156f417aac0ebb55d14bf
haolinzhu <haolinz@berkeley.edu>
2021-04-17 02:52:48 -0700

    added last_accessed_at field

--- hknweb/shortlinks/views.py
+++ hknweb/shortlinks/views.py
@@ -12,6 +12,7 @@ def openLink(request, temp):
     count = Link.objects.filter(active=True, name=temp).count()
     if count == 0:
         return render(request, './404.html')
-    redirectLink = Link.objects.filter(active=True).get(name=temp)    
+    redirectLink = Link.objects.filter(active=True).get(name=temp)
+    redirectLink.access_time_now()
     link = redirectLink.redirect
     return redirect(link)
