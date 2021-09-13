a0c4bebaf3a63baceb3c81e9856d438ddf438888
bri25yu <bri25yu@gmail.com>
2021-09-12 21:44:54 -0700

    applying black formatter to hknweb/settings

--- hknweb/settings/dummy_secrets.py
+++ hknweb/settings/dummy_secrets.py
@@ -1,3 +1,3 @@
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v9lj^szduvr@a*31&r(l5ub+5q%ebszts70vlpzaiekt23s)gb'
-SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
+SECRET_KEY = "v9lj^szduvr@a*31&r(l5ub+5q%ebszts70vlpzaiekt23s)gb"
+SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
