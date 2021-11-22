0a0f476b349136cc02330f1e0e5d63a55220cb13
Bryan Li <bryanli0@icloud.com>
2021-11-22 15:10:31 -0800

    Finished Task 3.

--- hknweb/polls/urls.py
+++ hknweb/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
--- hknweb/polls/views.py
+++ hknweb/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
 from .models import Question
 
@@ -11,10 +11,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
