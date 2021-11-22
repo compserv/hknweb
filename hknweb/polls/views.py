3377c23329d1f07a2b242f4ca6484f3da71b23d7
Daniel Cooper <dan.cooper@berkeley.edu>
2021-11-22 09:50:34 -0800

    refactoring to generic views, updated urls.py already

--- hknweb/polls/views.py
+++ hknweb/polls/views.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-from hknweb.polls.models import Question
+from hknweb.polls.models import Question, Choice
 
 
-def index(request):
+def indexView(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
         'latest_question_list': latest_question_list,
@@ -18,10 +20,21 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "Your're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "you didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 # Create your views here.
