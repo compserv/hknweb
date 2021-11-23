767212a4e98bbd0c3d269cf1854360d4b88f3a56
Maxwell Lo <lo-maxwell@berkeley.edu>
2021-11-22 22:22:11 -0800

    deleted extraneous commented code, removed mysite directory

--- hknweb/polls/views.py
+++ hknweb/polls/views.py
@@ -8,28 +8,6 @@ from .models import Choice, Question
 
 # Create your views here.
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     # return HttpResponse(template.render(context, request))
-#     return render(request, 'polls/index.html', context)
-#
-# def detail(request, question_id):
-#     # try:
-#     #     question = Question.objects.get(pk=question_id)
-#     # except Question.DoesNotExist:
-#     #     raise Http404("Question does not exist")
-#     # return render(request, 'polls/detail.html', {'question': question})
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-#
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
-
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
