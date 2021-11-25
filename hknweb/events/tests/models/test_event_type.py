00238dd612cee6c1dd5c006e80af233dd68236c3
bri25yu <bri25yu@gmail.com>
2021-11-25 16:43:10 -0700

    adding event type tests to enforce consistent behavior

---
+++ hknweb/events/tests/models/test_event_type.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+from hknweb.events.tests.models.utils import ModelFactory
+
+
+class EventModelTests(TestCase):
+    def setUp(self):
+        event_type = ModelFactory.create_event_type()
+
+        self.event_type = event_type
+
+    def test_repr(self):
+        expected = "EventType(type={})".format(self.event_type.type)
+        actual = repr(self.event_type)
+
+        self.assertEqual(expected, actual)
+
+    def test_str(self):
+        expected = str(self.event_type.type)
+        actual = str(self.event_type)
+
+        self.assertEqual(expected, actual)
