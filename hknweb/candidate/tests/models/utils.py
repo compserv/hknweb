d797921692de827c37d56edaf63b5066e64cbb06
bri25yu <bri25yu@gmail.com>
2021-11-28 12:38:49 -0700

    adding candidate bitbyteactivity activity model tests

--- hknweb/candidate/tests/models/utils.py
+++ hknweb/candidate/tests/models/utils.py
@@ -1,2 +1,30 @@
+from django.contrib.auth.models import User
+
+from hknweb.candidate.models import BitByteActivity
+
+
 class ModelFactory:
-    pass
+    @staticmethod
+    def create_user(**kwargs):
+        default_kwargs = {
+            "username": "default username",
+        }
+        kwargs = {**default_kwargs, **kwargs}
+        return User.objects.create(**kwargs)
+
+    @staticmethod
+    def create_bitbyteactivity_activity(participants, **kwargs):
+        default_kwargs = {
+            "proof": "default proof",
+            "notes": "default notes",
+        }
+        kwargs = {
+            **default_kwargs,
+            **kwargs,
+        }
+        bitbyteactivity = BitByteActivity.objects.create(**kwargs)
+
+        bitbyteactivity.participants.add(*participants)
+        bitbyteactivity.save()
+
+        return bitbyteactivity
