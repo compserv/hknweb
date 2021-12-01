3b3c46841beefe3d584fd54387ba4ac4f0b6db83
Bryan Li <bryanli0@icloud.com>
2021-11-30 16:09:28 -0800

    Fixed admin page, changed template

--- hknweb/polls/admin.py
+++ hknweb/polls/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [(None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']})]
@@ -9,9 +13,5 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
 
 admin.site.register(Question, QuestionAdmin)
--- hknweb/polls/models.py
+++ hknweb/polls/models.py
@@ -11,6 +11,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now >= self.pub_date >= now - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
