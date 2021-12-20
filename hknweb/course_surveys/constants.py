d1e466232ec3429e3f628266ebc6e2ba86c90832
bri25yu <bri25yu@gmail.com>
2021-12-19 21:32:55 -0800

    adding course surveys transparency index

---
+++ hknweb/course_surveys/constants.py
@@ -0,0 +1,18 @@
+class Attr:
+    NAME = "name"
+    PATH = "path"
+    PAGES = "pages"
+
+
+COURSE_SURVEY_TRANSPARENCY_PAGE_PATHS = [
+    "course_surveys_authentication",
+    "course_surveys_infrastructure",
+    "course_surveys_upload",
+    "course_surveys_visualization",
+    "course_surveys_database",
+    "course_surveys_management",
+    "course_surveys_permissions",
+    "course_surveys_search",
+]
+
+COURSE_SURVEY_PREFIX = "Course surveys "
