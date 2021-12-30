242e4685bf2b3f4c8d171d882f2bc940ff7e7708
bri25yu <46059916+bri25yu@users.noreply.github.com>
2021-12-29 18:19:55 -0700

    Course surveys frontend-backend integration (#369)
    * adding cas login to session
    
    * adding course surveys index title; integrating front + back end; improving css
    
    * adding permissions to academics api
    
    * adding ratings display to frontend

--- hknweb/course_surveys/constants.py
+++ hknweb/course_surveys/constants.py
@@ -1,14 +1,32 @@
 class Attr:
+    COLOR = "color"
+    COURSE = "course"
+    COURSE_ID = "course_id"
     COURSES = "courses"
     DEPT = "dept"
     FORMAT = "format"
+    ID = "id"
+    INSTRUCTOR = "instructor"
+    INSTRUCTOR_TYPE = "instructor_type"
     INSTRUCTORS = "instructors"
+    MAX = "max"
     NAME = "name"
+    NEXT_PAGE = "next_page"
+    NUM_STUDENTS = "num_students"
     NUMBER = "number"
     PATH = "path"
     PAGES = "pages"
+    PREVIOUS_PAGE = "previous_page"
+    RATINGS = "ratings"
+    RESPONSE_COUNT = "response_count"
+    SECTION_NUMBER = "section_number"
+    SEMESTER = "semester"
     SERVICE = "service"
+    SURVEY = "survey"
+    SURVEY_NUMBER = "survey_number"
+    TEXT = "text"
     TICKET = "ticket"
+    VALUE = "value"
 
 
 COURSE_SURVEY_TRANSPARENCY_PAGE_PATHS = [
@@ -26,7 +44,14 @@ COURSE_SURVEY_PREFIX = "Course surveys "
 
 
 class CAS:
-    SERVICE_VALIDATE_URL = "https://auth.berkeley.edu/cas/serviceValidate"
     AUTHENTICATION_SUCCESS = "authenticationSuccess"
     JSON = "JSON"
+    SIGNED_IN = "signed_in"
     SERVICE_RESPONSE = "serviceResponse"
+    SERVICE_VALIDATE_URL = "https://auth.berkeley.edu/cas/serviceValidate"
+
+
+class COLORS:
+    FIRE_BRICK = "FireBrick"
+    FOREST_GREEN = "ForestGreen"
+    GOLDEN_ROD = "GoldenRod"
--- hknweb/course_surveys/views.py
+++ hknweb/course_surveys/views.py
@@ -8,6 +8,7 @@ from hknweb.academics.models import Course, Instructor
 from hknweb.course_surveys.constants import (
     Attr,
     CAS,
+    COLORS,
     COURSE_SURVEY_PREFIX,
     COURSE_SURVEY_TRANSPARENCY_PAGE_PATHS,
 )
@@ -17,14 +18,35 @@ class IndexView(TemplateView):
     template_name = "course_surveys/index.html"
 
     def get_context_data(self, **kwargs):
-        service = self.request.build_absolute_uri("?")
+        context = {}
+
+        service = self.request.build_absolute_uri()
         cas_signed_in = self._validate_cas(service)
 
+        survey_number = int(self.request.GET.get(Attr.SURVEY_NUMBER, 1))
+
+        course, _context = self._get_course(
+            cas_signed_in,
+            self.request.GET.get(Attr.COURSE, None),
+            survey_number,
+        )
+        context = {**context, **_context}
+
+        instructor, _context = self._get_instructor(
+            cas_signed_in,
+            self.request.GET.get(Attr.INSTRUCTOR, None),
+            survey_number,
+        )
+        context = {**context, **_context}
+
         context = {
+            **context,
             Attr.PAGES: self._get_pages(),
             Attr.SERVICE: service,
             Attr.COURSES: self._get_courses(cas_signed_in),
             Attr.INSTRUCTORS: self._get_instructors(cas_signed_in),
+            Attr.COURSE: course,
+            Attr.INSTRUCTOR: instructor,
         }
         return context
 
@@ -32,6 +54,10 @@ class IndexView(TemplateView):
         """
         See https://apereo.github.io/cas/6.4.x/protocol/CAS-Protocol.html
         """
+        cas_signed_in = self.request.session.get(CAS.SIGNED_IN, None)
+        if cas_signed_in:
+            return True
+
         ticket = self.request.GET.get(Attr.TICKET, None)
         if ticket is None:
             return False
@@ -46,7 +72,9 @@ class IndexView(TemplateView):
         content = json.loads(response.content.decode("utf-8"))
         response = content[CAS.SERVICE_RESPONSE]
 
-        return CAS.AUTHENTICATION_SUCCESS in response
+        self.request.session[CAS.SIGNED_IN] = CAS.AUTHENTICATION_SUCCESS in response
+
+        return self.request.session[CAS.SIGNED_IN]
 
     @staticmethod
     def _get_courses(cas_signed_in: bool):
@@ -59,7 +87,13 @@ class IndexView(TemplateView):
             if not course.icsr_course.exists():
                 continue
 
-            most_recent_icsr = course.icsr_course.latest(
+            icsrs = course.icsr_course.filter(
+                section_number__exact="1", instructor_type__exact="Professor"
+            )
+            if not icsrs.exists():
+                continue
+
+            most_recent_icsr = icsrs.latest(
                 "icsr_semester__year",
                 "-icsr_semester__year_section",
             )
@@ -76,6 +110,8 @@ class IndexView(TemplateView):
                 {
                     Attr.DEPT: most_recent_icsr.icsr_department.abbr,
                     Attr.NUMBER: most_recent_icsr.course_number,
+                    Attr.NAME: most_recent_icsr.course_name,
+                    Attr.ID: course.id,
                 }
             )
 
@@ -110,6 +146,7 @@ class IndexView(TemplateView):
             instructors.append(
                 {
                     Attr.NAME: name,
+                    Attr.ID: instructor.instructor_id,
                 }
             )
 
@@ -133,3 +170,108 @@ class IndexView(TemplateView):
                 )
 
         return pages
+
+    @staticmethod
+    def _get_course(cas_signed_in, id, survey_number):
+        if not cas_signed_in or id is None:
+            return None, {}
+
+        course = Course.objects.get(pk=id)
+
+        icsrs = course.icsr_course.order_by(
+            "-icsr_semester__year",
+            "icsr_semester__year_section",
+        ).filter(section_number__exact="1", instructor_type__exact="Professor")
+        most_recent_icsr = icsrs.first()
+
+        if survey_number > len(icsrs):
+            return None, {}
+        icsr = icsrs[survey_number - 1]
+
+        course_context = {
+            Attr.DEPT: most_recent_icsr.icsr_department.abbr,
+            Attr.NUMBER: most_recent_icsr.course_number,
+            Attr.NAME: most_recent_icsr.course_name,
+            Attr.ID: course.id,
+        }
+        context = {
+            Attr.SURVEY: IndexView._get_survey(icsr),
+            Attr.PREVIOUS_PAGE: survey_number - 1 if survey_number > 1 else None,
+            Attr.NEXT_PAGE: survey_number + 1 if survey_number < len(icsrs) else None,
+        }
+        return course_context, context
+
+    @staticmethod
+    def _get_instructor(cas_signed_in, id, survey_number):
+        if not cas_signed_in or id is None:
+            return None, {}
+
+        instructor = Instructor.objects.get(pk=id)
+
+        icsrs = instructor.icsr_instructor.order_by(
+            "-icsr_semester__year",
+            "icsr_semester__year_section",
+        )
+        most_recent_icsr = icsrs.first()
+
+        if survey_number > len(icsrs):
+            return None, {}
+        icsr = icsrs[survey_number - 1]
+
+        name = "{first_name} {last_name}".format(
+            first_name=most_recent_icsr.first_name,
+            last_name=most_recent_icsr.last_name,
+        )
+
+        instructor_context = {
+            Attr.NAME: name,
+            Attr.ID: instructor.instructor_id,
+            Attr.INSTRUCTOR_TYPE: most_recent_icsr.instructor_type,
+        }
+        context = {
+            Attr.SURVEY: IndexView._get_survey(icsr),
+            Attr.PREVIOUS_PAGE: survey_number - 1 if survey_number > 1 else None,
+            Attr.NEXT_PAGE: survey_number + 1 if survey_number < len(icsrs) else None,
+        }
+        return instructor_context, context
+
+    @staticmethod
+    def _get_survey(icsr):
+        semester = icsr.icsr_semester
+        survey = icsr.survey_icsr.first()
+
+        ratings = []
+        for rating in survey.rating_survey.all():
+            percent = rating.rating_value / rating.range_max
+            color = COLORS.FIRE_BRICK
+            if rating.inverted:
+                if percent < 0.2:
+                    color = COLORS.FOREST_GREEN
+                elif percent < 0.4:
+                    color = COLORS.GOLDEN_ROD
+            else:
+                if percent > 0.8:
+                    color = COLORS.FOREST_GREEN
+                elif percent > 0.6:
+                    color = COLORS.GOLDEN_ROD
+
+            ratings.append(
+                {
+                    Attr.TEXT: rating.question_text,
+                    Attr.MAX: rating.range_max,
+                    Attr.VALUE: rating.rating_value,
+                    Attr.COLOR: color,
+                }
+            )
+
+        return {
+            Attr.DEPT: icsr.icsr_department.abbr,
+            Attr.NUMBER: icsr.course_number,
+            Attr.COURSE_ID: icsr.icsr_course.id,
+            Attr.SECTION_NUMBER: icsr.section_number,
+            Attr.SEMESTER: semester.year_section + str(semester.year)[-2:],
+            Attr.INSTRUCTOR_TYPE: icsr.instructor_type,
+            Attr.NUM_STUDENTS: survey.num_students,
+            Attr.RESPONSE_COUNT: survey.response_count,
+            Attr.RATINGS: ratings,
+        }
