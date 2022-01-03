b7e9e420705d0f2d252d4c55ff5b0b9ec0e5300d
bri25yu <46059916+bri25yu@users.noreply.github.com>
2022-01-03 09:07:15 -0700

    Adding combine_surveys action and adding required serializers (#373)
    * adding combine_surveys action
    
    * adding pagination to drf api
    
    * adding survey_icsr and rating_survey related fields to serializer

--- hknweb/academics/serializers/course_surveys/survey.py
+++ hknweb/academics/serializers/course_surveys/survey.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
 from hknweb.academics.models import Survey, ICSR
+from hknweb.academics.serializers.course_surveys.rating import RatingSerializer
 
 
 class SurveySerializer(serializers.HyperlinkedModelSerializer):
     survey_icsr = serializers.HyperlinkedRelatedField(
         view_name="academics:icsr-detail", queryset=ICSR.objects.all()
     )
+    rating_survey = RatingSerializer(many=True, read_only=True)
 
     class Meta:
         model = Survey
@@ -17,6 +19,7 @@ class SurveySerializer(serializers.HyperlinkedModelSerializer):
             "num_students",
             "response_count",
             "is_private",
+            "rating_survey",
         ]
         extra_kwargs = {
             "url": {"view_name": "academics:survey-detail"},
