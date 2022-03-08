06ac77caf83a15b05b11784e82a6ca68170986f6
bri25yu <bri25yu@gmail.com>
2022-03-07 17:37:42 -0800

    moving forms deeper

---
+++ hknweb/events/forms/__init__.py
@@ -0,0 +1,2 @@
+from hknweb.events.forms.event import EventForm, EventUpdateForm
+from hknweb.events.forms.attendance import AttendanceFormForm, AttendanceResponseForm
---
+++ hknweb/events/forms/event/__init__.py
@@ -0,0 +1,2 @@
+from hknweb.events.forms.event.create import EventForm
+from hknweb.events.forms.event.update import EventUpdateForm
