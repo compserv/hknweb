9457dd7da56c4bf04ab320144d7c6f87df238871
bri25yu <bri25yu@gmail.com>
2022-03-08 18:20:36 -0800

    adding req_info data encapsulation

--- hknweb/candidate/utils_candportal/candidate_portal_data.py
+++ hknweb/candidate/utils_candportal/candidate_portal_data.py
@@ -13,10 +13,7 @@ from hknweb.candidate.models import (
     RequirementMergeRequirement,
 )
 
-from hknweb.candidate.utils_candportal.utils import (
-    create_title,
-    get_requirement_colors,
-)
+from hknweb.candidate.utils_candportal.utils import create_title
 from hknweb.candidate.utils_candportal.check_requirements import (
     check_requirements,
 )
@@ -35,6 +32,7 @@ from hknweb.candidate.utils_candportal.process_misc_req import (
     CommitteeProjectProcessor,
     DuePaymentProjectProcessor,
 )
+from hknweb.candidate.utils_candportal.req_info import ReqInfo
 
 
 """ What the event types are called on admin site.
@@ -65,7 +63,7 @@ class CandidatePortalData:
         num_challenges_confirmed,
         num_bitbytes,
         req_list,
-    ):
+    ) -> ReqInfo:
         def get_events_by_confirmed(confirmed):
             return get_events(
                 rsvps,
@@ -87,64 +85,58 @@ class CandidatePortalData:
             req_list,
         )
 
-        return confirmed_events, unconfirmed_events, req_statuses, req_remaining
+        return ReqInfo(
+            req_list,
+            req_statuses,
+            req_remaining,
+            confirmed_events,
+            unconfirmed_events,
+        )
 
-    def process_merge_node(
-        self,
-        node,
-        req_titles,
-        req_remaining,
-        req_list,
-        req_colors,
-        req_statuses,
-        confirmed_events,
-        unconfirmed_events,
-        merge_names,
-    ):
+    def process_merge_node(self, node, req_info) -> str:
         node_string = node.get_events_str()
         remaining_count, grand_total = 0, 0
 
         node_string_key = node_string
         count = 2
-        if node_string_key in req_titles:
-            while node_string_key in req_titles:
+        if node_string_key in req_info.titles:
+            while node_string_key in req_info.titles:
                 node_string_key = "{} {}".format(node_string, count)
                 count += 1
-            req_colors[node_string_key] = req_colors[node_string]
+            req_info.colors[node_string_key] = req_info.colors[node_string]
 
-        req_statuses[node_string_key] = True
+        req_info.statuses[node_string_key] = True
         if node.all_required:
             grand_total = -1
             for event in node.events():
-                req_statuses[node_string_key] = (
-                    req_statuses[node_string_key] and req_statuses[event]
+                req_info.statuses[node_string_key] = (
+                    req_info.statuses[node_string_key] and req_info.statuses[event]
                 )
-                if not req_statuses[node_string_key]:
+                if not req_info.statuses[node_string_key]:
                     break
         else:
-            remaining_count, grand_total = node.get_counts(req_remaining, req_list)
-            req_statuses[node_string_key] = round(remaining_count, 2) < 0.05
+            remaining_count, grand_total = node.get_counts(req_info.remaining, req_info.lst)
+            req_info.statuses[node_string_key] = round(remaining_count, 2) < 0.05
 
         # num_required_hangouts is None, since Merger nodes should not use it
         if node.all_required:
             # TODO Support for All Required for Merged Requirement (probably not a huge priority)
-            req_titles[node_string_key] = (
+            req_info.titles[node_string_key] = (
                 node_string
                 + " - Looped Merged Requirements for all required currently unsupported"
             )
         else:
-            req_titles[node_string_key] = create_title(
+            req_info.titles[node_string_key] = create_title(
                 "", remaining_count, node_string, grand_total, None
             )
 
-        confirmed_events[node_string_key] = []
-        unconfirmed_events[node_string_key] = []
+        req_info.confirmed_events[node_string_key] = []
+        req_info.unconfirmed_events[node_string_key] = []
         for event in node.events():
-            confirmed_events[node_string_key].extend(confirmed_events[event])
-            unconfirmed_events[node_string_key].extend(unconfirmed_events[event])
+            req_info.confirmed_events[node_string_key].extend(req_info.confirmed_events[event])
+            req_info.unconfirmed_events[node_string_key].extend(req_info.unconfirmed_events[event])
 
-        merge_names.append(node_string_key)
-        # req_statuses, confirmed_events, unconfirmed_events
+        return node_string_key
 
     def get_user_cand_data(self):
         candidate_semester = self.user.profile.candidate_semester
@@ -234,12 +226,7 @@ class CandidatePortalData:
         ###
 
         # Process Events here
-        (
-            confirmed_events,
-            unconfirmed_events,
-            req_statuses,
-            req_remaining,
-        ) = self.process_events(
+        req_info = self.process_events(
             Rsvp.objects.filter(user__exact=self.user),
             required_events,
             candidate_semester,
@@ -249,63 +236,30 @@ class CandidatePortalData:
             req_list,
         )
 
+        req_info.set_titles(required_events)
         event_types = list(get_required_events(candidate_semester, None))
-        req_colors = get_requirement_colors(event_types)
-
-        blank_dict = {}
-        req_titles = {}
-        for req_type in req_statuses:
-            name = required_events.get(req_type, blank_dict).get("title", req_type)
-            if (name is None) or (name == ""):
-                name = req_type
-            title_created = create_title(
-                req_type,
-                req_remaining[req_type],
-                name,
-                req_list[req_type],
-                req_list.get(settings.HANGOUT_EVENT, blank_dict),
-            )
-            req_titles[req_type] = title_created
+        req_info.set_colors(event_types, merger_nodes)
 
         # Process Merged Events here
-        req_colors.update(
-            get_requirement_colors(
-                merger_nodes,
-                lambda view_key: view_key,
-                lambda get_key: get_key.get_events_str(),
-            )
-        )
-        merge_names = []
-        for node in merger_nodes:
-            self.process_merge_node(
-                node,
-                req_titles,
-                req_remaining,
-                req_list,
-                req_colors,
-                req_statuses,
-                confirmed_events,
-                unconfirmed_events,
-                merge_names,
-            )
+        merge_names = [self.process_merge_node(node, req_info) for node in merger_nodes]
 
         events = [
             {
-                ATTR.TITLE: req_titles[t],
-                ATTR.STATUS: req_statuses[t],
-                ATTR.COLOR: req_colors[t],
-                ATTR.CONFIRMED: confirmed_events[t],
-                ATTR.UNCONFIRMED: unconfirmed_events[t]
+                ATTR.TITLE: req_info.titles[t],
+                ATTR.STATUS: req_info.statuses[t],
+                ATTR.COLOR: req_info.colors[t],
+                ATTR.CONFIRMED: req_info.confirmed_events[t],
+                ATTR.UNCONFIRMED: req_info.unconfirmed_events[t]
             }
         for t in (event_types + merge_names)]
 
         interactivities = {
-            ATTR.TITLE: req_titles[settings.HANGOUT_EVENT][
+            ATTR.TITLE: req_info.titles[settings.HANGOUT_EVENT][
                 settings.EITHER_ATTRIBUTE_NAME
             ],
-            ATTR.STATUS: req_statuses[settings.HANGOUT_EVENT],
+            ATTR.STATUS: req_info.statuses[settings.HANGOUT_EVENT],
             settings.CHALLENGE_ATTRIBUTE_NAME: {
-                ATTR.TITLE: req_titles[settings.HANGOUT_EVENT][
+                ATTR.TITLE: req_info.titles[settings.HANGOUT_EVENT][
                     settings.CHALLENGE_ATTRIBUTE_NAME
                 ],
                 ATTR.NUM_PENDING: num_pending,
@@ -314,7 +268,7 @@ class CandidatePortalData:
                 ATTR.NUM_CONFIRMED: num_challenges_confirmed,
             },
             settings.HANGOUT_ATTRIBUTE_NAME: {
-                ATTR.TITLE: req_titles[settings.HANGOUT_EVENT][
+                ATTR.TITLE: req_info.titles[settings.HANGOUT_EVENT][
                     settings.HANGOUT_ATTRIBUTE_NAME
                 ],
             },
@@ -322,12 +276,12 @@ class CandidatePortalData:
 
         bitbyte = {
             ATTR.TITLE: "Bit-Byte",
-            ATTR.STATUS: req_statuses[settings.BITBYTE_ACTIVITY],
+            ATTR.STATUS: req_info.statuses[settings.BITBYTE_ACTIVITY],
             ATTR.NUM_BITBYTES: num_bitbytes,
         }
 
         context = {
-            "req_statuses": {e: req_statuses[e] for e in event_types},
+            "req_statuses": {e: req_info.statuses[e] for e in event_types},
             "events": events,
             "interactivities": interactivities,
             "bitbyte": bitbyte,
@@ -335,9 +289,9 @@ class CandidatePortalData:
         context.update(self._get_misc_context(self.user, candidate_semester))
         context.update(self._get_event_related_context(
             self.user,
-            confirmed_events,
-            unconfirmed_events,
-            event_types
+            req_info.confirmed_events,
+            req_info.unconfirmed_events,
+            event_types,
         ))
         context.update(self._get_misc_req_related_context(
             self.user, candidate_semester
---
+++ hknweb/candidate/utils_candportal/req_info.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+
+from hknweb.candidate.utils_candportal.utils import (
+    create_title,
+    get_requirement_colors,
+)
+
+
+class ReqInfo:
+    def __init__(self, lst, statuses, remaining, confirmed_events, unconfirmed_events):
+        self.lst = lst
+        self.statuses = statuses
+        self.remaining = remaining
+        self.confirmed_events = confirmed_events
+        self.unconfirmed_events = unconfirmed_events
+
+        self.titles = None
+        self.colors = None
+
+    def set_titles(self, required_events):
+        titles = {}
+        for req_type in self.statuses:
+            name = required_events.get(req_type, {}).get("title", req_type)
+            if not name:
+                name = req_type
+
+            titles[req_type] = create_title(
+                req_type,
+                self.remaining[req_type],
+                name,
+                self.lst[req_type],
+                self.lst.get(settings.HANGOUT_EVENT, {}),
+            )
+
+        self.titles = titles
+
+    def set_colors(self, event_types, merger_nodes):
+        colors = get_requirement_colors(event_types)
+        colors.update(
+            get_requirement_colors(
+                merger_nodes,
+                lambda view_key: view_key,
+                lambda get_key: get_key.get_events_str(),
+            )
+        )
+
+        self.colors = colors
