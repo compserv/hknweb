38e147a0f279e7268bf03c0296dad3f50aaecf1f
bri25yu <bri25yu@gmail.com>
2022-04-02 14:00:43 -0700

    fixing course surveys index login error

--- hknweb/course_surveys/tests/test_index.py
+++ hknweb/course_surveys/tests/test_index.py
@@ -2,8 +2,15 @@ from django.test import TestCase
 
 from django.urls import reverse
 
+from hknweb.course_surveys.tests.utils import (
+    create_user_with_course_surveys_edit_permission,
+)
+
 
 class IndexViewTests(TestCase):
+    def setUp(self):
+        create_user_with_course_surveys_edit_permission(self)
+
     def test_returns_200(self):
         response = self.client.get(reverse("course_surveys:index"))
 
