fd724a6e1a48a9efc428c9194514356643fa2512
bri25yu <bri25yu@gmail.com>
2022-04-05 00:36:49 -0700

    adding tests for checkoffs

--- hknweb/candidate/tests/views/utils.py
+++ hknweb/candidate/tests/views/utils.py
@@ -8,13 +8,28 @@ from hknweb.init_permissions import provision
 class CandidateViewTestsBase(TestCase):
     def setUp(self):
         password = "custom password"
-        candidate = ModelFactory.create_user(username="test_candidate")
+        candidate = ModelFactory.create_user(
+            username="test_candidate",
+            email="test_candidate@berkeley.edu",
+            first_name="test",
+            last_name="candidate",
+        )
         candidate.set_password(password)
         candidate.save()
-        officer = ModelFactory.create_user(username="test_officer1")
+        officer = ModelFactory.create_user(
+            username="test_officer1",
+            email="test_officer1@berkeley.edu",
+            first_name="test",
+            last_name="officer1",
+        )
         officer.set_password(password)
         officer.save()
-        officer2 = ModelFactory.create_user(username="test_officer2")
+        officer2 = ModelFactory.create_user(
+            username="test_officer2",
+            email="test_officer2@berkeley.edu",
+            first_name="test",
+            last_name="officer2",
+        )
         officer2.set_password(password)
         officer2.save()
 
