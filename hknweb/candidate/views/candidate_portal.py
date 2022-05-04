31394c62f1c9c4762d176fe5e67264c7e5300e9f
bri25yu <bri25yu@gmail.com>
2022-05-03 17:46:58 -0700

    adding event_req information

--- hknweb/candidate/views/candidate_portal.py
+++ hknweb/candidate/views/candidate_portal.py
@@ -1,4 +1,11 @@
+from django.utils import timezone
 from django.shortcuts import render
+
+from hknweb.candidate.models import Announcement, Logistics
+from hknweb.events.models import Event, Rsvp
+from hknweb.coursesemester.models import Semester
+
+from hknweb.utils import get_access_level
 
 from hknweb.utils import (
     GROUP_TO_ACCESSLEVEL,
@@ -8,5 +15,29 @@ from hknweb.utils import (
 
 @login_and_access_level(GROUP_TO_ACCESSLEVEL["candidate"])
 def candidate_portal(request):
-    context = {}
+    today = timezone.now()
+    upcoming_events = Event.objects \
+        .filter(
+            start_time__range=(today, today + timezone.timedelta(days=7)),
+            access_level__gte=get_access_level(request.user),) \
+        .order_by("start_time")
+
+    semester = Semester.objects.order_by("year", "-semester").last()
+    logistics = Logistics.objects.filter(semester=semester).first()
+
+    event_reqs = logistics.event_reqs.all()
+    rsvps = Rsvp.objects.filter(user=request.user)
+    for event_req in event_reqs:
+        event_req_rsvps = rsvps.filter(event__event_type__in=event_req.event_types.all())
+        event_req.confirmed = [r.event for r in event_req_rsvps.filter(confirmed=True)]
+        event_req.unconfirmed = [r.event for r in event_req_rsvps.filter(confirmed=False)]
+        event_req.n_finished = len(event_req.confirmed)
+
+    context = {
+        "user_self": True,
+        "username": request.user.username,
+        "announcements": Announcement.objects.filter(visible=True).order_by("-release_date"),
+        "upcoming_events": upcoming_events,
+        "event_reqs": event_reqs,
+    }
     return render(request, "candidate/candidate_portal.html", context=context)
