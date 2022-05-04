5f958fe9a076b0d121d1a6d0ac2f24ec3c7238d3
bri25yu <bri25yu@gmail.com>
2022-05-04 13:09:52 -0700

    moving logistics populate code for a single user; making logistics time sensitive

--- hknweb/candidate/models/logistics.py
+++ hknweb/candidate/models/logistics.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from hknweb.events.models import EventType
+from hknweb.events.models import EventType, Rsvp
 from hknweb.coursesemester.models import Semester
+
+from hknweb.candidate.models import OffChallenge, BitByteActivity
 from hknweb.candidate.models.constants import MAX_STRLEN, MAX_TXTLEN
 
 
@@ -55,3 +57,40 @@ class Logistics(models.Model):
 
     misc_reqs = models.ManyToManyField(MiscReq, blank=True)
     form_reqs = models.ManyToManyField(FormReq, blank=True)
+
+    def populate(self, user: User):
+        rsvps = Rsvp.objects \
+            .filter(user=user, event__start_time__range=[self.date_start, self.date_end])
+
+        self.event_req_objs = self.event_reqs.all()
+        for event_req in self.event_req_objs:
+            event_req_rsvps = rsvps.filter(event__event_type__in=event_req.event_types.all())
+            event_req.confirmed = [r.event for r in event_req_rsvps.filter(confirmed=True)]
+            event_req.unconfirmed = [r.event for r in event_req_rsvps.filter(confirmed=False)]
+            event_req.n_finished = len(event_req.confirmed)
+        self.event_reqs_finished = all(e.n_finished >= e.n for e in self.event_req_objs)
+
+        self.forms_confirmed = self.form_reqs.filter(completed__in=[user])
+        self.forms_unconfirmed = self.form_reqs.exclude(completed__in=[user])
+        self.misc_confirmed = self.misc_reqs.filter(completed__in=[user])
+        self.misc_unconfirmed = self.misc_reqs.exclude(completed__in=[user])
+
+        challenges = OffChallenge.objects \
+            .filter(requester=user, request_date__range=[self.date_start, self.date_end])
+        self.n_challenges_confirmed = sum(c.confirmed for c in challenges)
+        self.n_challenges_rejected = sum(c.rejected for c in challenges)
+        self.n_challenges_pending = \
+            challenges.count() - self.n_challenges_confirmed - self.n_challenges_rejected
+
+        hangouts = rsvps.filter(event__event_type__type="Hangout")
+        self.hangouts_confirmed = [r.event for r in hangouts.filter(confirmed=True)]
+        self.hangouts_unconfirmed = [r.event for r in hangouts.filter(confirmed=False)]
+
+        self.n_interactivities = len(self.hangouts_confirmed) + self.n_challenges_confirmed
+
+        self.n_bitbyte = BitByteActivity.objects \
+            .filter(
+                participants__in=[user],
+                confirmed=True,
+                request_date__range=[self.date_start, self.date_end],
+            ).count()
--- hknweb/candidate/views/candidate_portal.py
+++ hknweb/candidate/views/candidate_portal.py
@@ -24,35 +24,7 @@ def candidate_portal(request):
 
     semester = Semester.objects.order_by("year", "-semester").last()
     logistics = Logistics.objects.filter(semester=semester).first()
-    rsvps = Rsvp.objects.filter(user=request.user)
-
-    logistics.event_req_objs = logistics.event_reqs.all()
-    for event_req in logistics.event_req_objs:
-        event_req_rsvps = rsvps.filter(event__event_type__in=event_req.event_types.all())
-        event_req.confirmed = [r.event for r in event_req_rsvps.filter(confirmed=True)]
-        event_req.unconfirmed = [r.event for r in event_req_rsvps.filter(confirmed=False)]
-        event_req.n_finished = len(event_req.confirmed)
-    logistics.event_reqs_finished = all(e.n_finished >= e.n for e in logistics.event_req_objs)
-
-    logistics.forms_confirmed = logistics.form_reqs.filter(completed__in=[request.user])
-    logistics.forms_unconfirmed = logistics.form_reqs.exclude(completed__in=[request.user])
-    logistics.misc_confirmed = logistics.misc_reqs.filter(completed__in=[request.user])
-    logistics.misc_unconfirmed = logistics.misc_reqs.exclude(completed__in=[request.user])
-
-    challenges = OffChallenge.objects.filter(requester=request.user)
-    logistics.n_challenges_confirmed = sum(c.confirmed for c in challenges)
-    logistics.n_challenges_rejected = sum(c.rejected for c in challenges)
-    logistics.n_challenges_pending = \
-        challenges.count() - logistics.n_challenges_confirmed - logistics.n_challenges_rejected
-
-    hangouts = rsvps.filter(event__event_type__type="Hangout")
-    logistics.hangouts_confirmed = [r.event for r in hangouts.filter(confirmed=True)]
-    logistics.hangouts_unconfirmed = [r.event for r in hangouts.filter(confirmed=False)]
-
-    logistics.n_interactivities = len(logistics.hangouts_confirmed) + logistics.n_challenges_confirmed
-
-    logistics.n_bitbyte = BitByteActivity.objects \
-        .filter(participants__in=[request.user], confirmed=True).count()
+    logistics.populate(request.user)
 
     context = {
         "user_self": True,
