5a738f7f510c7fa50eec32d3b4614ca1cac09cd2
bri25yu <bri25yu@gmail.com>
2022-05-05 20:27:49 -0700

    condense bitbyte proof link display

--- hknweb/candidate/templatetags/app_filters.py
+++ hknweb/candidate/templatetags/app_filters.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 from django import template
 
 register = template.Library()
@@ -6,3 +8,8 @@ register = template.Library()
 @register.filter(name="has_group")
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+
+
+@register.filter(name="is_link")
+def is_link(s: str):
+    return urlparse(s).netloc
