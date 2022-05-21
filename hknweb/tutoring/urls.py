3dd6ed34f2963fcd9dd53542ca0c61ab4f01224b
bri25yu <bri25yu@gmail.com>
2022-05-21 15:09:58 -0700

    removing tutoring files to begin anew

--- hknweb/tutoring/urls.py
+++ hknweb/tutoring/urls.py
@@ -5,12 +5,4 @@ from . import views
 app_name = "tutoring"
 urlpatterns = [
     path(r"", views.index, name="index"),
-    path(r"generate", views.generate_schedule, name="generate"),
-    path(r"slotpref", views.tutor_slot_preference, name="slotpref"),
-    path(r"coursepref", views.tutor_course_preference, name="coursepref"),
-    path(
-        r"prepare-algorithm-input",
-        views.prepare_algorithm_input,
-        name="prepare-algorithm-input",
-    ),
 ]
