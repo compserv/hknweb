e09b8397d9874d8688b491c1d60adacca3fc7dab
bri25yu <bri25yu@gmail.com>
2022-05-27 15:00:31 -0700

    implementing swapper

---
+++ hknweb/tutoring/scheduler/swapper.py
@@ -0,0 +1,62 @@
+import random
+
+from typing import List
+
+from hknweb.tutoring.scheduler.weighting import Weighting
+from hknweb.tutoring.scheduler.data import Data
+from hknweb.tutoring.scheduler.tutoring import Slot, Tutor
+from hknweb.tutoring.scheduler.evaluator import Evaluator
+
+
+class Swapper:
+    ITERATIONS_MULTIPLIER: int = 100000
+    THRESHOLD: float = 1e-10
+    SEED = 42
+
+    @staticmethod
+    def stabilize(data: Data, weighting: Weighting) -> None:
+        random.seed(Swapper.SEED)
+
+        iterations: int = len(data.tutors) * Swapper.ITERATIONS_MULTIPLIER
+        curr_best: float = Evaluator.evaluate(data, weighting)[1]
+
+        for q in range(5):
+            # Less movement the more iterations we go
+            cthresh: float = Swapper.THRESHOLD / ((10 - q) ** 2)
+
+            for k in range(5, 1, -1):
+                # Do k-way swaps iter times
+                for _ in range(iterations):
+                    # Choose k distinct tutors with nonempty slots
+                    nonempty_tutors: List[Tutor] = [t for t in data.tutors if t.slots]
+                    to_swap: List[Tutor] = random.sample(nonempty_tutors, k)
+
+                    # Choose a random slot within each tutor
+                    slots = [random.choice(t.slots) for t in to_swap]
+                    slots_rotated_right = slots[1:] + slots[:1]
+
+                    # Now do a circular swap
+                    for swap in zip(to_swap, slots, slots_rotated_right):
+                        Swapper._swap(*swap)
+
+                    # Only move up if at least thresh improvement
+                    cost: float = Evaluator.evaluate(data, weighting)[1]
+                    if cost > (curr_best + cthresh):
+                        print(f"Moved up by {cost - curr_best:.6f}")
+                        curr_best = cost
+                    else:   # Undo changes otherwise
+                        for swap in zip(to_swap, slots_rotated_right, slots):
+                            Swapper._swap(*swap)
+
+    @staticmethod
+    def _swap(tutor: Tutor, slot1: Slot, slot2: Slot) -> None:
+        """
+        Swap tutor from slot1 to slot2
+        """
+        # Remove slot1 from tutor
+        slot1.unassign(tutor)
+        tutor.unassign(slot1)
+
+        # Assign slot2 to tutor
+        slot2.assign(tutor)
+        tutor.assign(slot2)
