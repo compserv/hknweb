5570b18990a07775cd8fe40a2e63cb8d692ba74d
bri25yu <bri25yu@gmail.com>
2022-05-31 14:25:20 -0700

    removing schedule original matching print output

--- hknweb/tutoring/scheduler/schedule.py
+++ hknweb/tutoring/scheduler/schedule.py
@@ -22,8 +22,7 @@ def schedule(data: Data, output_readable=True, weighting_str: str="gardener") ->
     while score < 0:
         # data.clear_assignments()  # TODO
         matcher.match()
-        std, score = Evaluator.evaluate(data, weighting)
-        print(f"Initial score: {score}, Stddev: {std}")
+        _, score = Evaluator.evaluate(data, weighting)
 
     # Now do some random swapping to make it stable
     Swapper.stabilize(data, weighting)
