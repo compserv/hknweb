bea6b9dc14300a00ec0b26ecfe71d773783f632e
bri25yu <bri25yu@gmail.com>
2022-05-31 14:51:04 -0700

    improving swapper

--- hknweb/tutoring/scheduler/swapper.py
+++ hknweb/tutoring/scheduler/swapper.py
@@ -2,6 +2,8 @@ import random
 
 from typing import List
 
+from itertools import product
+
 from hknweb.tutoring.scheduler.weighting import Weighting
 from hknweb.tutoring.scheduler.data import Data
 from hknweb.tutoring.scheduler.tutoring import Slot, Tutor
@@ -9,59 +11,46 @@ from hknweb.tutoring.scheduler.evaluator import Evaluator
 
 
 class Swapper:
-    ITERATIONS_MULTIPLIER: int = 100000
-    THRESHOLD: float = 1e-10
-    Q = 5
-    K_high = 5
-    K_low = 2
+    ITERATIONS_MULTIPLIER: int = 10000  # Originally 100000 * 5 (for removed Q value)
+    Ks = list(range(5, 2-1, -1))
 
     @staticmethod
     def stabilize(data: Data, weighting: Weighting) -> None:
         iterations: int = len(data.tutors) * Swapper.ITERATIONS_MULTIPLIER
         curr_best: float = Evaluator.evaluate(data, weighting)[1]
 
-        i, total = 0, Swapper.Q * (Swapper.K_high - Swapper.K_low + 1) * iterations
         nonempty_tutors: List[Tutor] = [t for t in data.tutors if t.slots]
-        for q in range(Swapper.Q):
-            # Less movement the more iterations we go
-            cthresh: float = Swapper.THRESHOLD / ((10 - q) ** 2)
 
-            for k in range(Swapper.K_high, Swapper.K_low - 1, -1):
-                # Do k-way swaps iter times
-                for _ in range(iterations):
-                    # Choose k distinct tutors with nonempty slots
-                    to_swap: List[Tutor] = random.sample(nonempty_tutors, k)
+        total = len(Swapper.Ks) * iterations
+        # Do k-way swaps iter times
+        for i, (k, _) in enumerate(product(Swapper.Ks, range(iterations))):
+            # Choose k distinct tutors with nonempty slots
+            to_swap: List[Tutor] = random.sample(nonempty_tutors, k)
 
-                    # Choose a random slot within each tutor
-                    slots = [random.choice(list(t.slots)) for t in to_swap]
-                    slots_rotated_right = slots[1:] + slots[:1]
+            # Choose a random slot within each tutor
+            slots = [random.choice(list(t.slots)) for t in to_swap]
+            slots_rotated_right = slots[1:] + slots[:1]
 
-                    # TODO make tutors and slots sets into lists
-                    # Now do a circular swap
-                    for swap in zip(to_swap, slots, slots_rotated_right):
-                        Swapper._swap(*swap)
+            Swapper._circular_swap(to_swap, slots, slots_rotated_right)
 
-                    # Only move up if at least thresh improvement
-                    cost: float = Evaluator.evaluate(data, weighting)[1]
-                    if cost > (curr_best + cthresh):
-                        curr_best = cost
-                    else:   # Undo changes otherwise
-                        for swap in zip(to_swap, slots_rotated_right, slots):
-                            Swapper._swap(*swap)
+            cost: float = Evaluator.evaluate(data, weighting)[1]
+            if cost > curr_best:
+                curr_best = cost
+            else:  # Undo changes otherwise
+                Swapper._circular_swap(to_swap, slots_rotated_right, slots)
 
-                    i += 1
-                    print(f"Swapping {i * 100 // total}%\r", end="", flush=True)
+            pct = (i + 1) * 100
+            if pct % total == 0:  # Print every 1%
+                print(f"Swapping {pct // total}%\r", end="", flush=True)
         print()
 
     @staticmethod
-    def _swap(tutor: Tutor, slot1: Slot, slot2: Slot) -> None:
-        """
-        Swap tutor from slot1 to slot2
-        """
-        # Remove slot1 from tutor
-        slot1.unassign(tutor)
-        tutor.unassign(slot1)
+    def _circular_swap(tutors: List[Tutor], slots_from: List[Slot], slots_to: List[Slot]) -> None:
+        for tutor, slot_from, slot_to in zip(tutors, slots_from, slots_to):
+            # Remove slot_from from tutor
+            slot_from.unassign(tutor)
+            tutor.unassign(slot_from)
 
-        # Assign slot2 to tutor
-        slot2.assign(tutor)
-        tutor.assign(slot2)
+            # Assign slot_to to tutor
+            slot_to.assign(tutor)
+            tutor.assign(slot_to)
