5d9a6a09881ff0cbf55c4ee1ba27f8b2fbe864aa
bri25yu <bri25yu@gmail.com>
2022-06-04 20:10:42 -0700

    moving course and tutor filtering inside form init

--- hknweb/tutoring/forms.py
+++ hknweb/tutoring/forms.py
@@ -8,19 +8,27 @@ from hknweb.tutoring.views.autocomplete import get_tutors
 
 class CourseFilterForm(forms.Form):
     course = forms.ModelMultipleChoiceField(
-        Course.objects.all(),
+        queryset=None,
         widget=autocomplete.ModelSelect2Multiple(
             url="tutoring:autocomplete_course",
             attrs={"onchange": "selector_decorator()"},
         ),
     )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["course"].queryset = Course.objects.all()
+
 
 class TutorFilterForm(forms.Form):
     tutor = forms.ModelMultipleChoiceField(
-        get_tutors(),
+        queryset=None,
         widget=autocomplete.ModelSelect2Multiple(
             url="tutoring:autocomplete_tutor",
             attrs={"onchange": "selector_decorator()"},
         ),
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["tutor"].queryset = get_tutors()
