2c873ba19fdbb4e936a6910619a90f0b53d01df5
ochan1 <oscar4856@yahoo.com>
2022-10-18 11:48:36 -0700

    Event timestamp fix + Test (#507)
    * Fix timestamp formats
    
    * Add Event Timestamp display tests
    
    * Format military hour to hour conversion

--- hknweb/events/templatetags/event_filters.py
+++ hknweb/events/templatetags/event_filters.py
@@ -2,6 +2,7 @@ from django import template
 import bleach
 from django.conf import settings
 from pytz import timezone
+from datetime import datetime
 
 register = template.Library()
 
@@ -11,13 +12,26 @@ def event_name(name):
     return bleach.clean(name, tags=[], strip=True)
 
 
+def military_hour_to_hour(hour):
+    assert (0 <= hour) and (hour <= 23), "Hour outside of military time"
+    if hour == 0:
+        # Midnight
+        return 12
+    elif hour <= 12:
+        # But more than 0
+        return hour
+    else:
+        # hour > 12
+        return hour - 12
+
+
 # Made like this to make it compadible with Windows + Linux
 #  Leading zero syntax on strftime has a difference ("#" and "-")
 def get_time_string(dt):
-    hour = dt.hour
+    hour = military_hour_to_hour(dt.hour)
     mins = dt.minute
     ampm = dt.strftime("%p")
-    return f"{hour}:{mins} {ampm}"
+    return f"{hour}:{mins:02} {ampm}"
 
 
 def get_date_string(dt):
@@ -29,14 +43,19 @@ def get_date_string(dt):
     return f"{weekday}, {month} {day}, {year} - {clock_time}"
 
 
+def get_event_timerange(start_time: datetime, end_time: datetime):
+    start_time_str = get_date_string(start_time)
+    end_time_str = ""
+    if start_time.date() == end_time.date():
+        end_time_str = get_time_string(end_time)
+    else:
+        end_time_str = get_date_string(end_time)
+    return "{} to {}".format(start_time_str, end_time_str)
+
+
 @register.filter
 def process_event_time(event):
     settings_time_zone = timezone(settings.TIME_ZONE)
-    start_time_entry = event.start_time.astimezone(settings_time_zone)
-    end_time_entry = event.end_time.astimezone(settings_time_zone)
-    start_date_time = get_date_string(start_time_entry)
-    if start_time_entry.date() == end_time_entry.date():
-        end_date_time = get_time_string(end_time_entry)
-    else:
-        end_date_time = get_date_string(end_time_entry)
-    return "{} to {}".format(start_date_time, end_date_time)
+    start_time = event.start_time.astimezone(settings_time_zone)
+    end_time = event.end_time.astimezone(settings_time_zone)
+    return get_event_timerange(start_time, end_time)
