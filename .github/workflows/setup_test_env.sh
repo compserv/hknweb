04623ede04f8bee8d909f895565c1f1afb658a4f
bri25yu <bri25yu@gmail.com>
2022-11-27 11:20:31 -0800

    github actions

--- .github/workflows/setup_test_env.sh
+++ .github/workflows/setup_test_env.sh
@@ -3,14 +3,11 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p "$HOME"/miniconda
 
-conda init bash
-source ~/.bashrc
+source "$HOME/miniconda/etc/profile.d/conda.sh"
 
-conda update -n base -c defaults conda
 conda info -a
 conda env create -f config/hknweb-dev.yml
 
 conda activate hknweb-dev
 
 python manage.py migrate
-echo $SHELL
