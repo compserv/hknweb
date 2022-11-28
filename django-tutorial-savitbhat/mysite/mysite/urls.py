52f59384c933af7b38e1422ed673102609d75412
Savit Bhat <bhatsavit@berkeley.edu>
2022-11-28 00:14:26 -0800

    pre generic view upgrades

--- django-tutorial-savitbhat/mysite/mysite/urls.py
+++ django-tutorial-savitbhat/mysite/mysite/urls.py
@@ -1,7 +1,7 @@
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import include, path
 
@@ -21,4 +20,3 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
-
