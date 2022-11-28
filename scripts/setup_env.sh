fc1e98a10ed591bab5d760e7da32ed8f6ebad60a
bri25yu <bri25yu@gmail.com>
2022-11-27 20:11:03 -0800

    once again not using source miniconda; using activate over conda activate

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -4,8 +4,7 @@ conda update -q -n base -c defaults conda
 conda env create -q -f config/"$1".yml
 conda info -a  # Print post-creation properties
 
-source ~/miniconda/etc/profile.d/conda.sh
-conda activate "$1"
+activate "$1"
 
 python manage.py migrate
 python manage.py collectstatic --noinput
