759894660fa4282cfa3371e3b37b6c946f5216bd
bri25yu <46059916+bri25yu@users.noreply.github.com>
2022-11-28 11:46:43 -0800

    Conda cleanup (#520)
    * cleaning up deps
    
    * debugging env
    
    * cleaning up fabfile
    
    * adding runs-on to all workflow files
    
    * not consolidating setup code in reusable workflow; using reusable shell script
    
    * pwd test in test_formatting
    
    * ls test dir
    
    * fixing run ls
    
    * fixing formatter test
    
    * testing out local deploy
    
    * black formatter
    
    * using custom deploy config class again
    
    * fixing missing deploy args
    
    * trying to fix deploy local
    
    * fixing run script with conda
    
    * moving items into scripts
    
    * fix setup test env call to setup env
    
    * using bash in setup test env
    
    * trying out different shebang input
    
    * no longer manually installing miniconda
    
    * once again sourcing miniconda profile
    
    * once again not using source miniconda; using activate over conda activate
    
    * only collecting static on hknweb-prod
    
    * fixing run.sh
    
    * fixing pathing error in test end to end
    
    * consolidating testing code
    
    * moving tests around; trying to match prod deployment in local tests
    
    * simplifying run_github_actions script to not use a socket
    
    * trying to run gunicorn headlessly
    
    * starting gunicorn headlessly in fabfile
    
    * trying out gunicorn daemon
    
    * curl actually outputs a failed exit code on fail

--- README.md
+++ README.md
@@ -1,101 +1,29 @@
 hknweb
 ======
 
-Welcome! This is the in-progress redesign for the HKN (Mu Chapter) website, built with Django.
-
-## Prerequisites
-These are the software you are **required** to download and install. Unless noted, default settings are OK. Consult with officers on advice for preferences of certain settings.
-
-There are other ways to install the following items. The instructions here will provide the EASIEST and SUFFICIENT way to install them. Ultimately, it is up to you on how to get them and install them if you want to do a more centalized way (i.e. Chocolatey for Windows) or a fancy pants method (i.e. Build from Scratch).
-
-When `Unix` is used, it includes (not limited to) Linux, Windows WSL, and MacOS
-
-* `git` and Terminal
-    * Unix systems usually have these out of the box
-        * Default terminal usually `zsh` or `bash`
-    * [Windows] [git bash](https://git-scm.com/downloads) **ONLY** (due to Makefile syntax)
-        * You can use the default options, **with one exception**: Select `Use Windows' default console window` in the **Configuring the terminal emulator** to use with Git Bash step. **Do not use "MinTTY"**.
-            * This is very important! If you do not select this option, your terminal won't work with Python!
-            * If you already have Git Bash installed before, just redownload and rerun the EXE (no need to uninstall, but you may if you want or need to) and go through all the options (it should keep what you previously selected). On the **Configuring the terminal emulator** page, select `Use Windows' default console window`.
-* `make` (GNU Make)
-    * Unix systems usually have this out of the box
-    * [Windows] Follow [instructions here using Winget](https://www.technewstoday.com/install-and-use-make-in-windows/#using-winget)
-        * **CORRECTION**: For **Steps 8 to 10**: Under `User variables` (top half section), click on `Path`, click on `Edit`, click on `New`, and enter `C:\Program Files (x86)\GnuWin32\bin`
-        * Article has other options too, but Winget is more out of the box. Next best thing is using Chocolatey which requires install of Chocolatey itself.
-* Miniconda (or Anaconda) + Python 3.7.3
-    * Our server will also use Miniconda to allow for some flexibility in installing packages without having to wait for OCF to install them for us, like upgrading the Python version. As of Fall 2022, we currently use Python 3.7.3.
-    * Install instructions here: [README-CONDA.md](README-CONDA.md)
-    * Commands Summary:
-        * Initial setup: `conda create -n hknweb python=<VERSION>`, where VERSION is the Python version used by `hknweb`. Use `make conda` instead which does this for you.
-        * Activate the conda environment with `conda activate hknweb` (do this every time you start developing on `hknweb`)
-        * Deactivate conda environment with `conda deactivate`
-        * Disable automatic `base` environment activation with `conda config --set auto_activate_base false`
+Welcome! This is the *in-progress website redesign* for the [IEEE-Eta Kappa Nu (HKN)](https://hkn.ieee.org/) [University of California, Berkeley Mu Chapter](https://dev-hkn.eecs.berkeley.edu/), built with [Django](https://www.djangoproject.com/), [Django REST framework](https://www.django-rest-framework.org/), and [Vue.js](https://vuejs.org/).
 
 ## Setup
+We use any `sh` shell e.g. `bash`, `zsh`, etc.
 
-Supported Terminals
-* Any Unix Terminals (including Windows WSL) -- usually `zsh` or `bash`
-* Windows Git Bash (**ONLY**) -- this is due to the Makefile syntax
+If `conda` is not already available, install [conda](https://docs.conda.io/projects/conda/en/stable/user-guide/install/index.html). If you're installing `conda` for the first time, we recommend `Miniconda` over `Anaconda`. 
 
-Developing on `hknweb` requires a virtual environment so that every developer has the exact same development environment (i.e. Any errors that a developer has is not due to difference in configuration). We will be using Conda Environments to make our virtual environment.
-
-Others exist like Python's built-in [`venv`](https://docs.python.org/3/library/venv.html), but we won't use that here
-
-This command creates our Conda Environment.
 ```sh
-$ make conda
+# Once `conda` is installed
+$ conda env create -f config/hknweb-dev.yml         # Create dev environment
+$ conda activate hknweb-dev                         # Activate dev environment
+$ python manage.py migrate                          # Initialize database
 ```
-
-Alternatively, this command creates our Conda Environment completely from scratch: `make conda-scratch`
-
-Next, we need to have our current terminal/shell use the Conda Environment we just created.
-```sh
-$ conda activate hknweb
-```
-
-Finally, we need to install all of our dependencies into the Conda Environment (and not the main computer system):
-```sh
-$ make install
-```
-
-In summary, the setup looks like:
-```sh
-$ cd hknweb                     # enter our main directory
-$ make conda                    # create our Conda Environment
-$ conda activate hknweb         # enter our Conda Environment
-$ make install                  # install our dependencies
-$ make migrate                  # apply all database changes
-$ make permissions              # initialize our database
-$ make dev                      # start local web server
-$ conda deactivate              # Exit the Conda Environment
-```
-
-You may notice many of our Makefile commands can be ran manually.
-
-We not only use Makefile commands to simplify and abstract away many commonly used development commands (especially setting the dev environment via `HKNWEB_MODE='dev'`), but also to sanity check to make sure we don't accidentally modify your main system (see how we call `python` in the Makefile).
 
 ## Development
+All commands we use are native to Django [link to available Django commands](https://docs.djangoproject.com/en/4.1/django-admin/#available-commands). If you haven't worked with Django before, checkout the [Django tutorial](https://docs.djangoproject.com/en/4.1/intro/).
 
-To run the Django development server (which runs a web server locally), run
+A few frequently used commands:
 ```sh
-$ make dev
+$ python manage.py makemigrations       # Create migrations files
+$ python manage.py migrate              # Apply migrations files to database
+$ python manage.py createsuperuser      # Create an admin super user to login as 
+$ python manage.py runserver            # Run development server
+$ coverage run                          # Run tests
+$ coverage report                       # Report test coverage
 ```
-which will make the web site available at `http://localhost:3000`.
-
-If you would like to access the admin interface in the local web server, run
-```sh
-$ make createsuperuser
-```
-
-You will be prompted for some login info, after which you should be able to access the admin interface with your super user credentials at `http://localhost:3000/admin`.
-
-Usually, Python code behavior between the Operating Systems is rare.
-However, Windows developers have the additional responsbility to make their code Linux-friendly (since our destination OS is Linux on OCF). Therefore, if there is an error on the code on Linux and not Windows, support on Linux takes precedence!
-Unix developers should also try to make their code Windows-friendly for the Window developers. [Example with `strftime`](https://github.com/compserv/hknweb/commit/3dc27dfa7556561b7c244de2c431d3c99ee2eb5a)
-
-## FAQ
-This is a compilation of past errors and how they were solved.
-
-### Python won't load on Windows Git Bash
-
-Git Bash's Terminal emulator should be `Use Windows' default console window`. Don't use MinTTY. Follow the instructions for reinstalling `Git Bash` in the **Prerequisites** section of this README. 
--- fabfile.py
+++ fabfile.py
@@ -1,28 +1,53 @@
-from fabric import Config
-from fabric import Connection
-from fabric import task
+import os
+
+import posixpath
+
+import json
+
+from fabric import Config, Connection, task
+
 from invoke import Collection
 from invoke.config import merge_dicts
 
-from deploy import git
-from deploy import path
 
-import argparse
-
-TARGET_FLAG = "--target"
-DEFAULT_TARGET = "prod"
-
-production_python = "HKNWEB_MODE=prod python"
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_DIR = os.path.join(ROOT_DIR, "config", "deploy")
 
 
-def timestamp(c: Connection) -> str:
-    """
-    Returns the server date-time, encoded as YYYYMMSS_HHMMSS.
-    """
-    return c.run("date +%Y%m%d_%H%M%S").stdout.strip()
+class ConfigFiles:
+    SHARED = os.path.join(CONFIG_DIR, "shared.json")
+    GITHUB_ACTIONS = os.path.join(CONFIG_DIR, "github_actions.json")
+    PROD = os.path.join(CONFIG_DIR, "prod.json")
 
 
-def create_dirs(c: Connection):
+class DeployConfig(Config):
+    @staticmethod
+    def global_defaults():
+        hkn_shared = json.load(open(ConfigFiles.SHARED))
+        return merge_dicts(Config.global_defaults(), hkn_shared)
+
+
+def setup(c: Connection, commit=None, release=None):
+    print("== Setup ==")
+
+    # Returns the server date-time, encoded as YYYYMMSS_HHMMSS.
+    timestamp = c.run("date +%Y%m%d_%H%M%S").stdout.strip()
+
+    # Point the connection to the correct git config
+    c.release = release if release else timestamp
+    c.commit = commit if commit else c.deploy.branch
+    print(f"release: {c.release}")
+    print(f"commit: {c.commit}")
+
+    # Setup paths
+    c.deploy_path = posixpath.join(c.deploy.path.root, c.deploy.name)
+    c.repo_path = posixpath.join(c.deploy_path, c.deploy.path.repo)
+    c.releases_path = posixpath.join(c.deploy_path, c.deploy.path.releases)
+    c.current_path = posixpath.join(c.deploy_path, c.deploy.path.current)
+    c.shared_path = posixpath.join(c.deploy_path, c.deploy.path.shared)
+    c.release_path = posixpath.join(c.releases_path, c.release)
+
+    # Create dirs
     dirs = (
         c.repo_path,
         c.deploy_path,
@@ -31,199 +56,129 @@ def create_dirs(c: Connection):
         c.release_path,
     )
     for d in dirs:
-        c.run("mkdir -p {}".format(d))
-
-
-class DeployConfig(Config):
-    @staticmethod
-    def global_defaults():
-        hkn_defaults = {
-            "deploy": {
-                "name": "default",
-                "user": "hkn",
-                "host": "apphost.ocf.berkeley.edu",
-                "path": {
-                    "root": "/home/h/hk/hkn/hknweb",
-                    "repo": "repo",
-                    "releases": "releases",
-                    "current": "current",
-                    "shared": "shared",
-                },
-                "repo_url": "https://github.com/compserv/hknweb.git",
-                "branch": "master",
-                "linked_files": [],
-                "linked_dirs": [],
-                "keep_releases": 10,
-            },
-        }
-        return merge_dicts(Config.global_defaults(), hkn_defaults)
-
-
-targets = {
-    "prod": {
-        "deploy": {
-            "name": "prod",
-            "branch": "master",
-        },
-    },
-}
-
-configs = {target: DeployConfig(overrides=config) for target, config in targets.items()}
-
-# pprint(vars(configs['prod']))
-
-
-def create_release(c: Connection):
-    print("-- Creating release")
-    git.check(c)
-    git.update(c)
-    c.commit = git.revision_number(c, c.commit)
-    git.create_archive(c)
-
-
-def symlink_shared(c: Connection):
-    print("-- Symlinking shared files")
-    with c.cd(c.release_path):
-        c.run("ln -s {}/media ./media".format(c.shared_path), echo=True)
-
-
-def decrypt_secrets(c):
-    print("-- Decrypting secrets")
-    with c.cd(c.release_path):
-        c.run("blackbox_postdeploy", echo=True)
-
-
-def install_deps(c: Connection):
-    print("-- Installing dependencies")
-    with c.cd(c.release_path):
-        with c.prefix("conda activate hknweb"):
-            c.run("make install-prod")
-
-
-def django_migrate(c: Connection):
-    print("-- Migrating tables")
-    with c.cd(c.release_path):
-        with c.prefix("conda activate hknweb"):
-            c.run("make check-conda-env")
-            c.run(f"{production_python} ./manage.py migrate")
-
-
-def django_collectstatic(c: Connection):
-    print("-- Collecting static files")
-    with c.cd(c.release_path):
-        with c.prefix("conda activate hknweb"):
-            c.run("make check-conda-env")
-            c.run(f"{production_python} ./manage.py collectstatic --noinput")
-
-
-def symlink_release(c: Connection):
-    print("-- Symlinking current@ to release")
-    c.run("ln -sfn {} {}".format(c.release_path, c.current_path), echo=True)
-
-
-def systemd_restart(c: Connection):
-    print("-- Restarting systemd unit")
-    c.run("systemctl --user restart hknweb.service", echo=True)
-
-
-def setup(c: Connection, commit=None, release=None):
-    print("== Setup ==")
-    if release is None:
-        c.release = timestamp(c)
-    else:
-        c.release = release
-    c.deploy_path = path.deploy_path(c)
-    c.repo_path = path.repo_path(c)
-    c.releases_path = path.releases_path(c)
-    c.current_path = path.current_path(c)
-    c.shared_path = path.shared_path(c)
-    c.release_path = path.release_path(c)
-    if commit is None:
-        c.commit = c.deploy.branch
-    else:
-        c.commit = commit
-    print("release: {}".format(c.release))
-    print("commit: {}".format(c.commit))
-    create_dirs(c)
-
-
-def create_conda(c: Connection):
-    with c.cd(c.release_path):
-        c.run("make conda")
+        c.run(f"mkdir -p {d}")
 
 
 def update(c: Connection):
     print("== Update ==")
-    create_release(c)
-    symlink_shared(c)
-    decrypt_secrets(c)
-    create_conda(c)
-    install_deps(c)
-    django_migrate(c)
-    django_collectstatic(c)
+
+    if c.deploy.use_local_repo:
+        c.deploy.repo_url = (
+            c.run("git config --get remote.origin.url").stdout.strip() + ".git"
+        )
+        c.commit = c.run("git rev-parse HEAD").stdout.strip()
+
+        c.run(f"git clone --bare {c.deploy.repo_url} {c.repo_path}", echo=True)
+
+    file_exists = lambda p: c.run(f"[[ -f {p} ]]", warn=True).ok
+    repo_exists = file_exists(f"{c.repo_path}/HEAD")
+
+    if repo_exists:  # fetch
+        with c.cd(c.repo_path):
+            c.run(f"git remote set-url origin {c.deploy.repo_url}", echo=True)
+            c.run("git remote update", echo=True)
+            c.run(f"git fetch origin {c.commit}:{c.commit}", echo=True)
+    else:  # clone
+        c.run(f"git clone --bare {c.deploy.repo_url} {c.repo_path}", echo=True)
+
+    with c.cd(c.repo_path):
+        print("-- Creating git archive for release")
+        revision = c.commit
+        revision_number = c.run(
+            f"git rev-list --max-count=1 {revision} --", echo=True
+        ).stdout.strip()
+        c.commit = revision_number
+
+        c.run(f"git archive {c.commit} | tar -x -f - -C '{c.release_path}'", echo=True)
+
+    with c.cd(c.release_path):
+        print("-- Symlinking shared files")
+        c.run(f"ln -s {c.shared_path}/media ./media", echo=True)
+
+        if c.deploy.run_blackbox_postdeploy:
+            print("-- Decrypting secrets")
+            c.run("blackbox_postdeploy", echo=True)
+        else:
+            print("-- Skipping decrypting secrets")
+
+    with c.cd(c.release_path):
+        print("-- Updating environment")
+        c.run(f"bash ./scripts/setup_env.sh {c.deploy.conda_env}", echo=True)
 
 
-def publish(c: Connection):
+def publish(c: Connection) -> None:
     print("== Publish ==")
-    symlink_release(c)
-    systemd_restart(c)
+
+    print("-- Symlinking current@ to release")
+    c.run(f"ln -sfn {c.release_path} {c.current_path}", echo=True)
+
+    print("-- Restarting systemd unit")
+    c.run("systemctl --user restart hknweb.service", echo=True)
 
 
-def finish(c):
-    pass
-
-
-# For the following @task functions, "target" is an ignored parameter
-#  It is a workaround to allow for use in using command line arguments
-#  For selecting a "target" in ns.configure
-# Otherwise, fabfile will claim to not recognize it
+"""
+For the following @task functions, "target" is an ignored parameter. It is a workaround to
+allow for use in using command line arguments for selecting a "target" in ns.configure. 
+Otherwise, fabfile will claim to not recognize it.
+"""
 
 
 @task
-def deploy(c, target=DEFAULT_TARGET, commit=None):
+def deploy(c, target=None, commit=None):
     with Connection(c.deploy.host, user=c.deploy.user, config=c.config) as c:
         setup(c, commit=commit)
         update(c)
         publish(c)
-        finish(c)
 
 
 @task
-def rollback(c, target=DEFAULT_TARGET, release=None):
+def rollback(c, target=None, release=None):
     with Connection(c.deploy.host, user=c.deploy.user, config=c.config) as c:
         setup(c, release=release)
         update(c)
         publish(c)
-        finish(c)
 
 
-# Please add the "target" parameter if you are adding more @task functions
-#  to allow custom targets to be used (regardless if your function itself will use it or not)
+@task
+def deploy_github_actions(c, target=None):
+    with Connection(c.deploy.host, user=c.deploy.user, config=c.config) as c:
+        c.run = c.local
+        setup(c)
+        update(c)
+
+        """
+        The local publish flow is slightly different than the default deploy publish flow.
+        We don't use systemctl to restart a service; instead, we use a custom run script
+        """
+        print("== Publish ==")
+
+        print("-- Symlinking current@ to release")
+        c.run(f"ln -sfn {c.release_path} {c.current_path}", echo=True)
+
+        print("-- Skipping restarting systemd unit")
+
+        with c.cd(c.current_path):
+            print("-- Starting server")
+            c.run(f"bash ./scripts/run_github_actions.sh {c.current_path}")
 
 
-def get_target(args):
-    target = args.target
-    if target not in configs:
-        message = '\n\tTarget Configuration "{}" is not a valid entry'.format(
-            TARGET_FLAG
-        )
-        message += "\n\tInvalid Entry: " + target
-        assert target in configs, message
-    return target
+def configure_namespace() -> Collection:
+    hknweb_mode = os.environ["HKNWEB_MODE"].lower()
+    if hknweb_mode == "dev":
+        config_file = ConfigFiles.GITHUB_ACTIONS
+        deploy = deploy_github_actions
+    elif hknweb_mode == "prod":
+        config_file = ConfigFiles.PROD
+    else:
+        raise ValueError(f"HKNWEB_MODE '{hknweb_mode}' is not a valid value")
+
+    config_dict = json.load(open(config_file))
+    config = DeployConfig(overrides=config_dict)
+
+    ns = Collection(deploy=deploy, rollback=rollback)
+    ns.configure(config)
+
+    return ns
 
 
-parser = argparse.ArgumentParser(
-    description='Target parameters for the fab file through the "fab" library'
-)
-parser.add_argument(
-    "--target",
-    default="prod",
-    help="The Target Configuration key to set the deployment setting",
-)
-args, unknown = parser.parse_known_args()
-
-target_key = get_target(args)
-print("Target Set:", target_key)
-
-ns = Collection(deploy, rollback)
-ns.configure(configs[target_key])
+ns = configure_namespace()
