b696144467339aaf295b23c6a4890c1b80ed56db
luisCermeno <luis.cermeno.f@gmail.com>
2022-12-01 15:19:01 -0800

    halfway through part 7

--- hknweb/polls/admin.py
+++ hknweb/polls/admin.py
@@ -3,5 +3,30 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    
+    # The following code is not supported by django 2.2.28
+    # @admin.display(
+    #     boolean=True,
+    #     ordering='pub_date',
+    #     description='Published recently?',
+    # )
+    # def was_published_recently(self):
+    #     now = timezone.now()
+    #     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
