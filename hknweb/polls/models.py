a4a9fa97f91a904d45575eb760f214db5282c862
Savit Bhat <bhatsavit@berkeley.edu>
2022-12-03 20:16:07 -0800

    candidate proj submission

--- hknweb/polls/models.py
+++ hknweb/polls/models.py
@@ -3,14 +3,19 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     def __str__(self):
         return self.question_text
-
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     def __str__(self):
