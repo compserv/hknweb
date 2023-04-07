a34121c251790fbbb963c4e99f5808f48c227661
williamtang.basis@gmail.com <williamtang.basis@gmail.com>
2023-04-06 17:23:59 -0700

    Integrate polls into hkn website

--- hknweb/polls/models.py
+++ hknweb/polls/models.py
@@ -7,12 +7,6 @@ from django.contrib import admin
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
-    @admin.display(
-        boolean=True,
-        ordering='pub_date',
-        description='Published recently?',
-    )
 
     def __str__(self):
         return self.question_text
@@ -20,6 +14,11 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
