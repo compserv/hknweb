14f40ee2f410dc45bc067bb8b9186a61dd4623b9
Michael Wiradharma <michael.wiradharma@berkeley.edu>
2023-04-25 11:59:52 -0700

    candidate project submission

---
+++ hknweb/polls/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+# Register your models here.
+admin.site.register(Question, QuestionAdmin)
