7d8d435d3e5105b48adc8ce4528f210feb2f2367
Yash Totale <totaleyash@gmail.com>
2023-04-25 15:40:07 -0700

    Finish part 5 of tutorial

--- hknweb/polls/models.py
+++ hknweb/polls/models.py
@@ -6,13 +6,14 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now()
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 datetime.timedelta(days=1)
