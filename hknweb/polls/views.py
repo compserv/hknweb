df1ed6c62b4647b66935fb1845f8a673d13f2ced
Michael Wiradharma <michael.wiradharma@berkeley.edu>
2023-04-25 11:59:52 -0700

    candidate project submission

---
+++ hknweb/polls/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Choice, Question
+
+# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including
+        those set to be published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:6]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any quesetions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+'''
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    # return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+'''
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1 
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
--- hknweb/urls.py
+++ hknweb/urls.py
@@ -27,6 +27,7 @@ app_urlpatterns = [
     path("tutoring/", include("hknweb.tutoring.urls")),
     path("cand/", include("hknweb.candidate.urls")),
     path("pages/", include("hknweb.markdown_pages.urls")),
+    path("polls/", include("hknweb.polls.urls")),
     path("course_surveys/", include("hknweb.course_surveys.urls")),
     path("", landing.home, name="home"),
     path("about/people/", people.people, name="people"),
