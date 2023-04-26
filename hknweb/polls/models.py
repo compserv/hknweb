ccf87cf81a430733fabdc6b6485d5d52772a6a91
Jiries Kaileh <mmjiries12@gmail.com>
2023-04-25 20:04:16 -0700

    integrated application to hknweb

--- hknweb/polls/models.py
+++ hknweb/polls/models.py
@@ -11,11 +11,6 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
-    @admin.display(
-        boolean=True,
-        ordering='pub_date',
-        description='Published recently?',
-    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
