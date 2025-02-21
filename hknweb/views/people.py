fc169ac89802ebb41d8e82e7dd7d7a4c2fedc66b
Jeffrey Tong <jeffrey.w.tong@gmail.com>
2025-02-20 17:13:36 -0800

    Add empty semester handling

--- hknweb/views/people.py
+++ hknweb/views/people.py
@@ -15,25 +15,37 @@ from hknweb.coursesemester.models import Semester
 def people(request):
     if "semester" in request.GET and not request.GET["semester"].isdigit():
         raise Http404
+
+    semesters_with_elections = Semester.objects.exclude(election=None)
+    
     semester: Semester = Semester.objects.filter(
         pk=request.GET.get("semester") or None
     ).first()
     if semester is None or not semester.election_set.exists():
         semester = (
-            Semester.objects.exclude(election=None)
+            semesters_with_elections
             .order_by("-year", "semester")
             .first()
         )
-    election: Election = semester.election_set.first()
+    
+    if semester == None:
+        officers = None
+    else:
+        election: Election = semester.election_set.first()
 
-    execs: QuerySet[Committeeship] = election.committeeship_set.filter(
-        committee__is_exec=True
-    ).order_by("committee__name")
-    committeeships: QuerySet[Committeeship] = election.committeeship_set.filter(
-        committee__is_exec=False
-    ).order_by("committee__name")
-
+        execs: QuerySet[Committeeship] = election.committeeship_set.filter(
+            committee__is_exec=True
+        ).order_by("committee__name")
+        committeeships: QuerySet[Committeeship] = election.committeeship_set.filter(
+            committee__is_exec=False
+        ).order_by("committee__name")
+        officers = {
+            "execs": execs,
+            "committeeships": committeeships
+        }
+    
     is_officer = get_access_level(request.user) <= GROUP_TO_ACCESSLEVEL["officer"]
+    
     form = ProfilePictureForm(request.POST)
     if is_officer and request.method == "POST":
         user = User.objects.get(pk=request.POST["user_id"])
@@ -42,10 +54,10 @@ def people(request):
             form.save()
 
     context = {
-        "execs": execs,
-        "committeeships": committeeships,
+        "officers": officers,
         "is_officer": is_officer,
         "form": form,
         "semester_select_form": SemesterSelectForm({"semester": semester}),
     }
+    
     return render(request, "about/people.html", context=context)
