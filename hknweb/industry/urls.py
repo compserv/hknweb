eff17cb449b7b9fb746f1e647fc78da0f8b7b05b
Enming Zhang <enming_zhang@berkeley.edu>
2025-04-27 12:00:23 -0700

    progress on updating industry visual: add all sub-pages

--- hknweb/industry/urls.py
+++ hknweb/industry/urls.py
@@ -6,4 +6,8 @@ import hknweb.industry.views as views
 app_name = "industry"
 urlpatterns = [
     path("what_is_hkn", views.what_is_hkn, name="what_is_hkn"),
+    path("resume_book", views.resume_book, name="resume_book"),
+    path("eecs_career_fair", views.eecs_career_fair, name="eecs_career_fair"),
+    path("infosessions", views.infosessions, name="infosessions"),
+    path("current_sponsors", views.current_sponsors, name="current_sponsors")
 ]
--- hknweb/industry/views.py
+++ hknweb/industry/views.py
@@ -14,3 +14,31 @@ def what_is_hkn(request):
         request,
         "industry/what_is_hkn.html",
     )
+
+@allow_public_access
+def resume_book(request):
+    return render(
+        request,
+        "industry/resume_book.html",
+    )
+
+@allow_public_access
+def eecs_career_fair(request):
+    return render(
+        request,
+        "industry/eecs_career_fair.html",
+    )
+
+@allow_public_access
+def infosessions(request):
+    return render(
+        request,
+        "industry/infosessions.html",
+    )
+
+@allow_public_access
+def current_sponsors(request):
+    return render(
+        request,
+        "industry/current_sponsors.html",
+    )
