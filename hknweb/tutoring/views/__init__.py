b059a7ff3b0b7081e74fcc0ca753e2d44d1d7d40
ryantrinh05 <156976151+ryantrinh05@users.noreply.github.com>
2025-05-01 21:11:51 -0700

    Course descriptions (#591)
    * Adding Course Edit Page
    
    * Created Committee Portal and Tutoring Page for Course Descriptions
    
    ---------------------
    
    Co-authored-by: Linus Torvalds <torvalds@linux-foundation.org>

--- hknweb/tutoring/views/__init__.py
+++ hknweb/tutoring/views/__init__.py
@@ -1,3 +1,5 @@
 from hknweb.tutoring.views.index import index
 from hknweb.tutoring.views.slots import slots
+from hknweb.tutoring.views.tutoringportal import tutoringportal
+from hknweb.tutoring.views.courses import courses
 from hknweb.tutoring.views.autocomplete import course_autocomplete, tutor_autocomplete
