621e43ee05d37543be3364fd1fb7eecbccdba04b
508312 <113813353+508312@users.noreply.github.com>
2026-01-11 23:27:43 -0800

    bitbyte view and upload

--- hknweb/candidate/admin/bit_byte_group.py
+++ hknweb/candidate/admin/bit_byte_group.py
@@ -1,10 +1,19 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.shortcuts import render, redirect
+from django.urls import path
+from django.db import transaction
+from django.contrib.auth.models import User
 
+import io
+import csv
+from hknweb.coursesemester.models import Semester
 from hknweb.candidate.models import BitByteGroup
-
+from hknweb.forms import CsvImportForm
 
 @admin.register(BitByteGroup)
 class BitByteGroupAdmin(admin.ModelAdmin):
+    change_list_template = "admin/candidate/bitbyte_group_change_list.html"
+
     fields = ["semester", "bytes", "bits"]
     list_display = (
         "semester",
@@ -28,3 +37,48 @@ class BitByteGroupAdmin(admin.ModelAdmin):
     def bits_usernames(self, obj):
         return ", ".join([user.username for user in obj.bits.all()])
 
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('import-csv/', self.import_csv, name="import_csv"),
+        ]
+        return custom_urls + urls
+
+    def import_csv(self, request):
+        if request.method == "POST":
+            form = CsvImportForm(request.POST, request.FILES)
+            if form.is_valid():
+                csv_file = request.FILES["csv_file"]
+                data_set = csv_file.read().decode('UTF-8')
+                io_string = io.StringIO(data_set)
+                next(io_string)  # Skip header
+
+                try:
+                    # If an error happens we just don't do anything
+                    with transaction.atomic():
+                        count = 0
+                        for row in csv.reader(io_string, delimiter=";"):
+                            if not row or len(row) != 4:
+                                continue
+
+                            byte_usernames, bit_usernames, year, sem = [s.strip() for s in row]
+
+                            semester_obj = Semester.objects.get(year=year, semester=sem)
+                            group = BitByteGroup.objects.create(semester=semester_obj)
+                            for byte in byte_usernames.split(","):
+                                byte_user = User.objects.filter(username__iexact=byte).first()
+                                group.bytes.add(byte_user)
+
+                            for bit in bit_usernames.split(","):
+                                bit_user = User.objects.filter(username__iexact=bit).first()
+                                group.bits.add(bit_user)
+
+                            count += 1
+                        self.message_user(request, f"Successfully imported {count} relationships.")
+                        return redirect("..")
+                except Exception as e:
+                    self.message_user(request, f"Error: {e}", level=messages.ERROR)
+
+        form = CsvImportForm()
+        payload = {"form": form}
+        return render(request, "admin/candidate/bitbyte_group_csv_upload.html", payload)
--- hknweb/views/bitbyte_tree.py
+++ hknweb/views/bitbyte_tree.py
@@ -1,46 +1,50 @@
 from django.shortcuts import render
 
+from hknweb.candidate.models import BitByteGroup
 from hknweb.utils import allow_public_access, login_and_committee
 from django.conf import settings
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 
+
 @login_and_committee(settings.COMPSERV_GROUP)
 def update_bitbyte_tree(request):
     return
 
-# TODO: dont read from the file when actual model and backend for bitbyte groups exists.
 @allow_public_access
 def bitbyte_tree_data(request):
-    byte_bits = {}
-    data = {"nodes": [], "links": []}
-    all_bytes = []
-    with open("hknweb/static/bit_byte_tree_data.csv", "r") as f:
-        lines = f.readlines()[1::]
-        for line in lines:
-            line = line.strip()
-            if line.split(",")[0] not in all_bytes:
-                all_bytes.append(line.split(",")[0])
-            if line.split(",")[1] not in all_bytes:
-                all_bytes.append(line.split(",")[1])
+    groups = BitByteGroup.objects.prefetch_related("bits", "bytes").all()
 
-            if not byte_bits.get(line.split(",")[0], False):
-                byte_bits[line.split(",")[0]] = []
-            byte_bits[line.split(",")[0]].append(line.split(",")[1])
+    username_to_node = {}
+    links = []
 
-    for byte in all_bytes:
-        user = User.objects.get(username__iexact=byte)
-        data["nodes"].append(
-            {
-                "id": byte,
-                "name": user.first_name + " " + user.last_name,
-                "candidate_semester": user.date_joined.year,
-            }
-        )
+    for group in groups:
+        group_bytes = group.bytes.all()
+        group_bits = group.bits.all()
 
-    for byte in byte_bits:
-        for bit in byte_bits[byte]:
-            data["links"].append({"source": byte, "target": bit})
+        def add_to_nodes(user_list):
+            for user in user_list:
+                if user.username not in username_to_node:
+                    username_to_node[user.username] = {
+                        "id": user.username,
+                        "name": f"{user.first_name} {user.last_name}",
+                        "candidate_semester": user.date_joined.year,
+                    }
+
+        add_to_nodes(group_bytes)
+        add_to_nodes(group_bits)
+
+        for byte in group_bytes:
+            for bit in group_bits:
+                links.append({
+                    "source": byte.username,
+                    "target": bit.username
+                })
+
+    data = {
+        "nodes": list(username_to_node.values()),
+        "links": links
+    }
 
     return JsonResponse(data)
 
